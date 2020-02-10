3990222fa34c212d73f466e8660241c59440ce97
shubhampal98 <shubhampal1898@gmail.com>
2020-02-10 19:00:20 +0530

    working on dashboard api

--- routes/report/dashboard/index.js
+++ routes/report/dashboard/index.js
@@ -1,11 +1,11 @@
 const dashboard = {
-    revenueExpenditure:require('./avenues-revenue-expenditure'),
-    cashAndBank:require('./cash-and-bank-balance'),
-    outstandingBank:require('./outstanding-debt'),
-    ownRevenueDependency:require('./own-revenue-dependency'),
-    sourceFinancialRevenueExpenditure:require('./source-financial-revenue-expenditure'),
-    sourceRevenue:require('./source-revenue'),
-    ulbCoverage:require('./ulb-coverage'),
-    filterUlbs:require('./filter-ulbs')
+    revenueExpenditure: require('./avenues-revenue-expenditure'),
+    cashAndBank: require('./cash-and-bank-balance'),
+    outstandingBank: require('./outstanding-debt'),
+    ownRevenueDependency: require('./own-revenue-dependency'),
+    sourceFinancialRevenueExpenditure: require('./source-financial-revenue-expenditure'),
+    sourceRevenue: require('./source-revenue'),
+    ulbCoverage: require('./ulb-coverage'),
+    filterUlbs: require('./filter-ulbs'),
 }
 module.exports = dashboard;
