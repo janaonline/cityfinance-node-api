40cc37af4b36c33d5196577567f003221ef1281a
kartik goyal <kartikhsr85@bitbucket.org>
2020-03-16 17:40:13 +0530

    ulb count list changes

--- routes/report/state-ulb-count.js
+++ routes/report/state-ulb-count.js
@@ -50,31 +50,77 @@ module.exports.getStateListWithCoveredUlb = async (req, res)=>{
             let obj = {}
             let ulbs = await Ulb.distinct("_id" , { state : el._id }).exec();
 
-            let cond = { ulb : { $in : ulbs }};
-            financialYear ? cond["financialYear"] = {$in: financialYear } : null;
-            let coveredUlbs = await UlbLedger.distinct("ulb",cond).exec(); 
+            let condition = { ulb : { $in : ulbs }};
+            financialYear ? condition["financialYear"] = {$in: financialYear } : null;
+            //let coveredUlbs = await UlbLedger.distinct("ulb",cond).exec(); 
+
             let overAllUlbs = await OverallUlb.distinct("_id",{ state : el._id }).exec();
 
-            cond["lineItem"] =  lineItem._id
-            let conditionForAudited = cond
-            conditionForAudited["amount"] = {$gt:0};
-            let audited = await UlbLedger.distinct("ulb",conditionForAudited).exec();
-
-            let conditionForNonAudited = cond
-            conditionForNonAudited["amount"] = {$eq:0};
-            let nonaudited = await UlbLedger.distinct("ulb",conditionForNonAudited).exec();
-
-            // let conditionForAuditNA = cond
-            // let AuditNA  = await UlbLedger.distinct("ulb",conditionForAuditNA).exec();
+            let data =  await UlbLedger.aggregate([
+                {$match : condition},
+                {$group:{
+                        _id: {
+                                ulb : "$ulb",
+                            },
+                            lineItem : {$addToSet : { _id : "$lineItem" ,amount:"$amount" } }
+                    }
+                },
+                {$project:{
+                       ulb : "$_id.ulb",
+                       lineItem :  {
+                          $filter: {
+                             input: "$lineItem",
+                             as: "lineItem",
+                             cond: { $and: [
+                                { $eq: [ "$$lineItem._id",lineItem._id] },
+                              ] }
+                          }
+                        }
+                    }
+                },
+                {$project:{
+                        "ulb" : 1,
+                        "lineItem" : { $arrayElemAt  :  [ "$lineItem",0]},
+                    }
+                },
+                {$project:{
+                        "ulb" : 1,
+                        amount  : "$lineItem.amount",
+                    }
+                },
+                {$project:{
+                        "ulb" : 1,
+                        auditStatus  : {
+                           $switch: {
+                              branches: [
+                                 { case: { $eq: [ "$amount", 0 ] }, then:"unaudited"},
+                                 { case: { $gt: [ "$amount", 0 ] }, then: "audited" }
+                              ],
+                              default:"auditNA"
+                           }
+                        },
+                    }
+                }
+            ])
             obj["code"] = el.code
             obj["name"] = el.name
             obj["_id"] = el._id
             obj["totalUlbs"] = overAllUlbs.length
-            obj["coveredUlbCount"] = coveredUlbs.length
+            obj["coveredUlbCount"] = 0
+            obj["audited"] = 0
+            obj["unaudited"] = 0
+            obj["auditNA"] = 0
+            data.map(m=> { 
+                obj["coveredUlbCount"]++;
+                if(m.auditStatus=="audited"){
+                    obj["audited"]++;
+                }else if(m.auditStatus=="nonaudited"){
+                    obj["unaudited"]++;
+                }else{
+                    obj["auditNA"]++; 
+                }
+            });
             obj["coveredUlbPercentage"] = (obj["coveredUlbCount"]/obj["totalUlbs"])*100 ?  ((obj["coveredUlbCount"]/obj["totalUlbs"])*100).toFixed(2) : 0
-            obj["audited"] = audited.length ;
-            obj["unaudited"] = nonaudited.length ;
-            obj["auditNA"] = 0 ;
             arr.push(obj);
         }
         return res.status(200).json({message: "State list with ulb covered percentage.", success: true, data:arr})
