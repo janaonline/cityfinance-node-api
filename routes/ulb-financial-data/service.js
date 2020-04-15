2ed5bfe24db3e754ab3ddb50681d0ade1e16f43f
amresh-yadav <amresh.yadav@dhwaniris.com>
2020-04-15 19:52:46 +0530

    resove

--- routes/ulb-financial-data/service.js
+++ routes/ulb-financial-data/service.js
@@ -444,7 +444,7 @@ module.exports.update = async (req, res)=>{
                 return Response.BadRequest(res,{}, "Requested record not found.")
             }else if(prevState.completeness == "REJECTED" || prevState.correctness == "REJECTED"){
                 for(let key of keys){
-                    if(data[key]){
+                    if(data[key] && typeof data[key] == 'object' && Object.keys(data[key]).length){
                         if(key == "auditReport" && prevState.audited){
                             Object.assign(prevState[key],data[key]);
                             prevState[key]["completeness"] = "PENDING";
