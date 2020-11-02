a1e04d5e88e34f58aa86df5a90267600ccf56bfc
arjun_dhwani <arjun.malik@dhwaniris.com>
2020-11-02 10:45:18 +0530

    issue fix CF-430

--- service/email-template.js
+++ service/email-template.js
@@ -210,7 +210,7 @@ const ulbSignupApproval = (sbCode,censusCode,name, link, edit = false) => {
                         <p>
                             Welcome to City Finance Portal! <br>
                             Your account has been successfully created. Please follow this link to set your password - <a href="${link}" target="_blank">link</a>.<br>
-                            Your Username is ${code}
+                            Your Username is ${sbCode} / ${censusCode}
 
                         </p>
                         <p>
