088c3a43bd14e36d461808844fd6b888cac7a6bf
arjun_dhwani <arjun.malik@dhwaniris.com>
2020-11-10 15:46:48 +0530

    name update script

--- routes/bulk-upload/index.js
+++ routes/bulk-upload/index.js
@@ -50,5 +50,6 @@ router.post('/bulk/ulb-upload',multerUpload.single('file'),BulkUpload.ulbUlpload
 router.post('/bulk/overall-ulb-upload',multerUpload.single('files'),BulkUpload.overallUlbUlpload);
 router.post("/bulk/ulb-location-update", multerUpload.single('csv'), BulkUpload.csvToJSON, BulkUpload.ulbLocationUpdate);
 router.post("/bulk/state-ulb-count-update", multerUpload.single('csv'), BulkUpload.csvToJSON,BulkUpload.stateUlbCountUpdate);
+router.post("/bulk/ulb-name-update", multerUpload.single('csv'), BulkUpload.csvToJSON, BulkUpload.ulbLocationUpdate.nameUpdate);
 
 module.exports = router;
--- routes/bulk-upload/ulb-location-update.js
+++ routes/bulk-upload/ulb-location-update.js
@@ -1,5 +1,6 @@
 const moment = require("moment");
 const Ulb = require('../../models/Ulb');
+const service = require("../../service");
 const requiredKeys = ["ULBCODE","LAT","LNG"];
 module.exports = async (req, res)=>{
     try {
@@ -27,6 +28,32 @@ module.exports = async (req, res)=>{
             failArr.push({message:"No row found."});
         }
         return res.status(200).json({success:true, data:failArr});
+    }catch (e) {
+        console.log("Exception:",e);
+        return res.status(500).json({message:e.message, success:false})
+    }
+};
+
+module.exports.nameUpdate = async (req, res)=>{
+    try {
+        const jsonArray = req.body.jsonArray;
+
+        for(let eachRow of jsonArray){
+
+            //console.log(eachRow.code,eachRow.name)
+            service.put({ code : eachRow.code },eachRow,Ulb,function(response,value){
+                if(!response){
+                    errors.push("Not able to create ulb => ",eachRow.code+""+response);
+                }
+                console.log(value.message);
+            });
+
+        }
+        return res.status(200).json({
+            message : "Successfully uploaded file",
+            success:true
+        })
+       
     }catch (e) {
         console.log("Exception:",e);
         return res.status(500).json({message:e.message, success:false})
