0dd6aa76ae7f21c8c5fa4cbbe1074f0ccf13f2d7
arjun_dhwani <arjun.malik@dhwaniris.com>
2020-11-21 13:30:59 +0530

    api rename

--- routes/ulb-financial-data/index.js
+++ routes/ulb-financial-data/index.js
@@ -19,6 +19,6 @@ router.put("/completeness/:_id",verifyToken, ufdService.completeness);
 router.get("/approved-records",verifyToken, ufdService.getApprovedFinancialData);
 router.get("/source-files/:_id",verifyToken, ufdService.sourceFiles);
 router.post("/action/:_id",verifyToken,ufdService.action);
-router.get("/fc-grant/dashboard/",verifyToken,ufdDashboardService);
+router.get("/fc-grant/dashboard-card/",verifyToken,ufdDashboardService);
 router.get("/fc-grant/dashboard-chart",verifyToken,ufdDashboardService.chartDataStatus);
 module.exports = router;
