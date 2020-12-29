c65c8e3d926f61b2368867a08937adfbce1d9c2f
arjun_dhwani <arjun.malik@dhwaniris.com>
2020-12-29 12:21:48 +0530

    bug-fix

--- routes/ulb-update-request/service.js
+++ routes/ulb-update-request/service.js
@@ -138,8 +138,8 @@ module.exports.create = async (req, res) => {
         }
 
         if(obj['censusCode']){
-            let ulbRecord = await Ulb.findOne({_id:{$nin:[ObjectId(ulb)]},censusCode:obj['censusCode']})
-            let censusRecord = await Ulb.findOne({_id:{$nin:[ObjectId(ulb)]},sbCode:obj['censusCode']})
+            let ulbRecord = await Ulb.findOne({censusCode:obj['censusCode']})
+            let censusRecord = await Ulb.findOne({sbCode:obj['censusCode']})
             if(ulbRecord || censusRecord){
                 return Response.BadRequest(
                     res,
@@ -149,8 +149,8 @@ module.exports.create = async (req, res) => {
             }
         }
         if(obj['sbCode']){
-            let ulbRecord = await Ulb.findOne({_id:{$nin:[ObjectId(ulb)]},sbCode:obj['sbCode']})
-            let censusRecord = await Ulb.findOne({_id:{$nin:[ObjectId(ulb)]},censusCode:obj['sbCode']})
+            let ulbRecord = await Ulb.findOne({sbCode:obj['sbCode']})
+            let censusRecord = await Ulb.findOne({censusCode:obj['sbCode']})
             if(ulbRecord || censusRecord){
                 return Response.BadRequest(
                     res,
