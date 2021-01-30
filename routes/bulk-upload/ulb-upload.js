1a3e48292276b4d9a2bd64bd45bd2334909233cb
arjun_dhwani <arjun.malik@dhwaniris.com>
2021-01-30 12:19:01 +0530

    changes

--- routes/bulk-upload/ulb-upload.js
+++ routes/bulk-upload/ulb-upload.js
@@ -104,7 +104,7 @@ module.exports = async function (req, res, next) {
                             eachRow['name'] = eachRow['ulbname'];
                             eachRow['code'] = eachRow.ulbcode;
                             eachRow['sbCode'] = eachRow.sbcode;
-                            eachRow['censusCode'] = eachRow.censusCode;
+                            eachRow['censusCode'] = eachRow.censuscode;
                             service.put(
                                 { code: eachRow.ulbcode },
                                 eachRow,
