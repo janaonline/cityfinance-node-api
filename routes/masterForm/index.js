b3d8d5bd598d909268d2458c63c3fa6bf9d01361
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-05-11 17:17:15 +0530

    get all masterform started

--- routes/masterForm/index.js
+++ routes/masterForm/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { verifyToken } = require('../auth/services/verifyToken')
 
 const { get } = require('./service')
+const { getAll } = require('./service')
 
 router.get('/get/:design_year', verifyToken, get)
+router.get('/getAll/:design_year', verifyToken, getAll)
 
 module.exports = router;
