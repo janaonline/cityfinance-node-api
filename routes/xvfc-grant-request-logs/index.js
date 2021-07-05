a120d9209404c2441ab763ce0f4c6cb1e8b52cb1
Sanju <sanju.pipal@dhwaniris.com>
2021-07-05 11:10:57 +0530

    delete api added , master changes, final/action changes

--- routes/xvfc-grant-request-logs/index.js
+++ routes/xvfc-grant-request-logs/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { saveLogs, getLogs } = require("./service");
+const { saveLogs, getLogs, allForms } = require("./service");
 const verifyToken = require("../auth/services/verifyToken").verifyToken;
 
 //create
@@ -8,5 +8,8 @@ router.post("/save-logs", verifyToken, saveLogs);
 
 //get
 router.get("/save-logs", verifyToken, getLogs);
+
+//
+router.post("/allFormsData/delete", allForms);
 
 module.exports = router;
