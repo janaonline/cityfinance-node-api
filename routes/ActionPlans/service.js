62e2d895f62fd92f58f31cc0c270d524964a3938
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-07-08 11:09:47 +0530

    bug fix

--- routes/ActionPlans/service.js
+++ routes/ActionPlans/service.js
@@ -28,7 +28,7 @@ exports.saveActionPlans = async (req, res) => {
 
 exports.getActionPlans = async (req, res) => {
   const { design_year } = req.params;
-  const state = req.decoded.state;
+  const state = req.query.state_id;
   try {
     const actionPlan = await ActionPlans.findOne({
       state: ObjectId(state),
