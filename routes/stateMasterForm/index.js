3977dc5dc7fb406967a9d1f13c51ba715dd71a1f
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-07-12 15:52:45 +0530

    bug fix

--- routes/stateMasterForm/index.js
+++ routes/stateMasterForm/index.js
@@ -5,7 +5,7 @@ const { verifyToken } = require('../auth/services/verifyToken')
 const { get } = require('./service')
 const { getAll } = require('./service')
 const { finalSubmit, finalAction } = require('./service')
-const { getHistory, getAllForms } = require('./service')
+const { getHistory, getAllForms, deleteForms } = require('./service')
 router.get('/get/:design_year', verifyToken, get) //state login
 router.get('/get/:design_year/:state_id', verifyToken, get) // admin login
 router.get('/getAllForms/:design_year/:state_id', verifyToken, getAllForms) // admin login
@@ -13,6 +13,7 @@ router.get('/getAll/:design_year', verifyToken, getAll)//MoHUA /admin login
 router.get('/history/:formId', verifyToken, getHistory)
 router.post('/finalSubmit', verifyToken, finalSubmit)
 router.post('/finalAction', verifyToken, finalAction)
+router.post('/deleteAllForms', deleteForms)
 
 
 module.exports = router;
