aaa0e9289ae59d6d26836706f0d8d47336555dd1
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-07-16 00:38:57 +0530

    mohua cards api

---
+++ routes/mohua-dashboard/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { verifyToken } = require('../auth/services/verifyToken')
+
+
+const { getCards, getForm, getTable } = require('./service')
+router.get('/cards', verifyToken, getCards)
+router.get('/forms', verifyToken, getForm)
+router.get('/table', verifyToken, getTable)
+
+
+
+module.exports = router;
