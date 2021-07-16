3f0ccd5cb6ca191710769f425fc59d021484f837
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-07-16 13:32:21 +0530

    form api for mohua dashboard

--- routes/mohua-dashboard/index.js
+++ routes/mohua-dashboard/index.js
@@ -5,7 +5,7 @@ const { verifyToken } = require('../auth/services/verifyToken')
 
 const { getCards, getForm, getTable } = require('./service')
 router.get('/cards', verifyToken, getCards)
-router.get('/forms', verifyToken, getForm)
+router.get('/forms/:design_year', verifyToken, getForm)
 router.get('/table', verifyToken, getTable)
 
 
