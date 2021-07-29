0fda947f1eca7612382aa5ee122e7c3dfefff843
Sanju <sanju.pipal@dhwaniris.com>
2021-07-29 17:22:36 +0530

    action plan excel

--- routes/ActionPlans/service.js
+++ routes/ActionPlans/service.js
@@ -4,7 +4,7 @@ const {
 } = require("../../service/updateStateMasterForm");
 const ObjectId = require("mongoose").Types.ObjectId;
 const Response = require("../../service").response;
-
+const ExcelJS = require("exceljs");
 exports.saveActionPlans = async (req, res) => {
   try {
     let { state, _id } = req.decoded;
@@ -95,4 +95,91 @@ exports.action = async (req, res) => {
     console.error(err.message);
     return Response.BadRequest(res, {}, err.message);
   }
+};
+
+exports.getExcel = async (req, res) => {
+  try {
+    const workbook = new ExcelJS.Workbook();
+    const { uaData, uaName } = req.body;
+    uaData.forEach((element) => {
+      createSheetForUa(workbook, element, uaName[element.ua].name);
+    });
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=" + "ActionPlanData.xlsx"
+    );
+    return workbook.xlsx.write(res).then(function () {
+      res.status(200).end();
+    });;
+  } catch (err) {
+    console.error(err.message);
+    return Response.BadRequest(res, {}, err.message);
+  }
+};
+
+let createSheetForUa = (workbook, uaData, sheetName) => {
+  workbook.addWorksheet(sheetName);
+  const worksheet = workbook.getWorksheet(sheetName);
+
+  worksheet.getCell("A1").value = "Projects";
+  let col = "",
+    colIndex = 0,
+    rowIndex = 2;
+  for (const key in uaData.projectExecute[0]) {
+    const element = uaData.projectExecute[0][key];
+    col = String.fromCharCode(colIndex++ + 65);
+    worksheet.getCell(col + 2).value = key;
+  }
+
+  uaData.projectExecute.forEach((object) => {
+    (col = ""), (colIndex = 0), (rowIndex++);
+    for (const key in object) {
+      const element = object[key];
+      col = String.fromCharCode(colIndex++ + 65);
+      worksheet.getCell(col + rowIndex).value = element;
+    }
+  });
+
+  (col = ""), (colIndex = 0), (rowIndex += 4);
+  worksheet.getCell("A" + rowIndex++).value = "Source Funds";
+  for (const key in uaData.sourceFund[0]) {
+    const element = uaData.sourceFund[0][key];
+    col = String.fromCharCode(colIndex++ + 65);
+    worksheet.getCell(col + rowIndex).value = key;
+  }
+  uaData.sourceFund.forEach((object) => {
+    (col = ""), (colIndex = 0), rowIndex++;
+    console.log(uaData);
+    for (const key in object) {
+      if(key == "ulb")
+      {
+        console.log("Ss");
+      }
+      const element = object[key];
+      col = String.fromCharCode(colIndex++ + 65);
+      worksheet.getCell(col + rowIndex).value = element;
+    }
+  });
+
+  (col = ""), (colIndex = 0), (rowIndex += 4);
+  worksheet.getCell("A" + rowIndex++).value = "Years Outlay";
+
+  for (const key in uaData.yearOutlay[0]) {
+    const element = uaData.yearOutlay[0][key];
+    col = String.fromCharCode(colIndex++ + 65);
+    worksheet.getCell(col + rowIndex).value = key;
+  }
+  uaData.yearOutlay.forEach((object) => {
+    (col = ""), (colIndex = 0), rowIndex++;
+    for (const key in object) {
+      const element = object[key];
+      col = String.fromCharCode(colIndex++ + 65);
+      worksheet.getCell(col + rowIndex).value = element;
+    }
+  });
 };
