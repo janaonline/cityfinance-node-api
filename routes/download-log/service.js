4d31f7e05dc5f01abafb2cfd0daf5c5f054818a3
Sanju <sanju.pipal@gmail.com>
2021-11-11 14:31:06 +0530

    pdf fix

--- routes/download-log/service.js
+++ routes/download-log/service.js
@@ -1,8 +1,6 @@
 const DownloadLog = require('../../models/DownloadLog');
 const fs = require('fs');
 const pdf = require('html-pdf');
-var Blob = require('blob');
-const axios = require('axios')
 const options = {
     format: 'A4',
     orientation: 'portrait',
@@ -37,22 +35,18 @@ module.exports.post = function(req, res){
 }
 
 
-module.exports.HtmlToPdf = async function (req, res){
+module.exports.HtmlToPdf = function(req, res){
+
     if (!req.body.html){
         res.status(400).json({success:false, message:"html Missing"});
         return;
     }
-    // let tt = await axios.get("https://staging-dhwani.s3.ap-south-1.amazonaws.com/GILAC_Non_CSR_-_Progress_Tracker_-_COVID-19__Utilisation_Certificate/pdf_2021-11-10%2018-06-15.html");
-    let temp = fs.createWriteStream("test.html").write(req.body.html);
-    var html = fs.readFileSync('routes/download-log/test.html', 'utf8');
-
-    pdf.create(html,options).toStream(function(err, stream) {
+    pdf.create(req.body.html,options).toStream(function(err, stream) {
         if(err){
             return res.status(400).json({success:false, message:"Something went wrong"})
         }else {
             stream.pipe(res);
         }
-
     });
 
 }
