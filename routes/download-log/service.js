7f0a4bf845c92758c291cffd983fbbba69d73aa4
Sanju <sanju.pipal@gmail.com>
2021-11-11 12:14:04 +0530

    code test

--- routes/download-log/service.js
+++ routes/download-log/service.js
@@ -38,21 +38,25 @@ module.exports.post = function(req, res){
 
 
 module.exports.HtmlToPdf = async function (req, res){
-    if (!req.body.html){
-        res.status(400).json({success:false, message:"html Missing"});
-        return;
-    }
-    // let tt = await axios.get("https://staging-dhwani.s3.ap-south-1.amazonaws.com/GILAC_Non_CSR_-_Progress_Tracker_-_COVID-19__Utilisation_Certificate/pdf_2021-11-10%2018-06-15.html");
-    let temp = fs.createWriteStream("test.html").write(req.body.html);
-    var html = fs.readFileSync('routes/download-log/test.html', 'utf8');
-
-    pdf.create(html,options).toStream(function(err, stream) {
-        if(err){
-            return res.status(400).json({success:false, message:"Something went wrong"})
-        }else {
-            stream.pipe(res);
+    try{
+        if (!req.body.html){
+            res.status(400).json({success:false, message:"html Missing"});
+            return;
         }
-
-    });
+        // let tt = await axios.get("https://staging-dhwani.s3.ap-south-1.amazonaws.com/GILAC_Non_CSR_-_Progress_Tracker_-_COVID-19__Utilisation_Certificate/pdf_2021-11-10%2018-06-15.html");
+        let temp = fs.createWriteStream("test.html").write(req.body.html);
+        var html = fs.readFileSync('routes/download-log/test.html', 'utf8');
+    
+        pdf.create(html,options).toStream(function(err, stream) {
+            if(err){
+                return res.status(400).json({success:false, message:"Something went wrong"})
+            }else {
+                stream.pipe(res);
+            }
+    
+        });
+    }catch(error){
+        return res.status(400).json({msg:error.msg,error})
+    }
 
 }
