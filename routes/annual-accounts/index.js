98027f4dc2916f947a683998393529668e152555
Ashu Gupta <ashu10832@gmail.com>
2022-03-06 07:07:48 +0530

    api for data sets in resource sectin

--- routes/annual-accounts/index.js
+++ routes/annual-accounts/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { verify } = require("jsonwebtoken");
 const router = express.Router();
 const { verifyToken } = require("../auth/services/verifyToken");
-const { getAccounts, action, createUpdate, getCSVAudited, getCSVUnaudited,nmpcEligibility, dashboard } = require("./service");
+const { getAccounts, action, createUpdate, getCSVAudited, getCSVUnaudited,nmpcEligibility, dashboard, dataset } = require("./service");
 const { userAuth } = require("../../middlewares/actionUserAuth");
 const statusList = require('../../util/newStatusList')
 // router.get('/get/:ulb', verifyToken, get);
@@ -13,5 +13,6 @@ router.get("/getCSV-Unaudited", getCSVUnaudited);
 router.get("/dashboard", dashboard);
 router.post("/create", verifyToken, createUpdate);
 router.post("/action", verifyToken, userAuth, action);
+router.get("/datasets", dataset);
 
 module.exports = router;
