696ea0d8b43d864781517a134f864c2814dc6d12
Ashu Gupta <ashu10832@gmail.com>
2022-06-11 12:22:11 +0530

    bug fix in state average

--- routes/newDashboards/city.js
+++ routes/newDashboards/city.js
@@ -983,11 +983,12 @@ const comparator = async (compareFrom, query, ulb, isPerCapita, from, body) => {
     case "ULB Population Category Average":
     case "ULB Type Average":
     case "National Average":
-    case "State ":
+    case "State "  :
+      case  "State Average" :
       if (compareFrom == "National Average") delete newData[0]?.$match?.ulb;
       else {
         let ids;
-        if (compareFrom == "State ")
+        if (compareFrom.includes('State'))
           ids = await Ulb.find({ state: ulbData.state })
             .select({ _id: 1 })
             .lean();
@@ -1124,7 +1125,8 @@ async function revenueExpenditureQueryCompare(
   let ulbId;
   let tempQ;
   switch (compareFrom) {
-    case "State ":
+    case "State ": 
+    case  'State Average':
       ulbData = await ULB.findOne({ _id: ulb }).lean();
       ulbId = await ULB.find({ state: ulbData.state }).lean();
       tempQ = [
@@ -1514,7 +1516,8 @@ async function revenueQueryCompare(
     lineItemIds = await LINEITEM.find({ headOfAccount: "Revenue" });
   }
   switch (compareFrom) {
-    case "State ":
+    case "State " : 
+    case 'State Average':
       ulbData = await ULB.findOne({ _id: ulb });
       ulbId = await ULB.find({ state: ulbData.state });
       tempQ = [
@@ -2249,7 +2252,8 @@ async function expenseQueryCompare(
     });
   } else lineItemIds = await LINEITEM.find({ headOfAccount: "Expense" });
   switch (compareFrom) {
-    case "State ":
+    case "State ": 
+    case  'State Average':
       ulbData = await ULB.findOne({ _id: ulb }).lean();
       ulbId = await ULB.find({ state: ulbData.state }).lean();
       ulbId = ulbId.map((val) => val._id);
@@ -3362,7 +3366,8 @@ const revenueIndicator = async (req, res) => {
     let compQuery = JSON.parse(JSON.stringify(query));
     if (compareType) {
       switch (compareType) {
-        case "State ":
+        case "State ": 
+        case 'State Average':
           compQuery.map(async (value) => {
             if (value["$match"]) {
               delete value["$match"].ulb;
