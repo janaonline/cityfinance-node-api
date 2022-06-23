847d93a2de09655069329ac660512b6695a4a5da
Aditya Yadav <aditya023.ay@gmail.com>
2022-06-23 13:02:35 +0530

    gfc-odf form collection initiated

--- routes/gfc-odf-form-collection/service.js
+++ routes/gfc-odf-form-collection/service.js
@@ -1,41 +1,42 @@
 const GfcFormCollection = require('../../models/GfcFormCollection');
+const OdfFormCollection = require('../../models/OdfFormCollection');
 const ObjectId = require("mongoose").Types.ObjectId;
 
-
-
-module.exports.createForm = async (req, res) => {
+module.exports.createorUpdateForm = async (req, res) => {
     try {
-
         const data = req.body;
         let user = req.decoded;
+        const isGfc = data.isGfc;  // flag to check which collection to use 
+        let collection = isGfc ? GfcFormCollection : OdfFormCollection;
         const { role: actionTakenByRole, _id: actionTakenBy, } = user;
         let formData = {}; //Object to store form data
-        formData['rating'] = data.rating;
+        formData['rating'] = ObjectId(data.rating);
         formData['cert'] = data.cert;
-        formData['certDate'] = data.certDate;
-        formData['ulb'] = data.ulb;
-        formData['year'] = data.year;
+        formData['certDate'] = new Date(data.certDate);
+        formData['ulb'] = ObjectId(data.ulb);
+        formData['year'] = ObjectId(data.year);
         formData['isDraft'] = data.isDraft;
         formData['status'] = data.status;
         formData['actionTakenByRole'] = actionTakenByRole;
-        formData['actionTakenBy'] = actionTakenBy;
+        formData['actionTakenBy'] = ObjectId(actionTakenBy);
         
         let condition = {}; // condition to find a document using ulb and year
         condition['ulb'] = ObjectId(data.ulb);
         condition['year'] = ObjectId(data.year);
-
-        let savedBody = new GfcFormCollection(formData);
+        let savedBody = new collection(formData);
         if (data.ulb && data.year) {
-            const form = await GfcFormCollection.findOne(condition);
+            const form = await collection.findOne(condition);
             if (form && (form.isDraft === false)) {//check if already exist and submitted
                 return res.status(200).json({
                     success: false,
-                    message: "Form already exist"
+                    message: "Form already submitted."
                 });
             }else if ((form === null) && (formData.isDraft === false)){//final submit in 1st attempt
-                const formSubmit = await GfcFormCollection.create(savedBody);
-                    if (formSubmit) {
-                        let updateData = await GfcFormCollection.updateOne(condition, 
+                const formSubmit = await collection.create(savedBody);
+                formData['createdAt'] = formSubmit.createdAt;
+                formData['modifiedAt'] = formSubmit.modifiedAt;
+                    if (formSubmit) {//add history
+                        let updateData = await collection.updateOne(condition, 
                             {
                                 $push: { history: formData},
                                 $set: formData,  
@@ -55,7 +56,7 @@ module.exports.createForm = async (req, res) => {
             }
         }
         if (data.isDraft){//update fields when isDraft===true 
-            const updateForm = await GfcFormCollection.updateOne(condition,
+            const updateForm = await collection.updateOne(condition,
                 formData,
                 { returnDocument: "after" });
             if(updateForm.n){
@@ -83,7 +84,10 @@ module.exports.createForm = async (req, res) => {
         }
         delete formData["history"]
         if (!data.isDraft){ //when form is submitted, save history
-            let updateData = await GfcFormCollection.updateOne(condition, 
+            const formSubmit = await collection.findOne(condition);
+            formData['createdAt'] = formSubmit.createdAt;
+                formData['modifiedAt'] = new Date();
+            let updateData = await collection.updateOne(condition, 
                 { $push: { history: formData}, $set: formData },//todo
                 { returnDocument: "after" });
             
@@ -103,8 +107,11 @@ module.exports.createForm = async (req, res) => {
 module.exports.getForm = async (req, res) => {
     try {
         const { ulbId, year } = req.body;
+        const isGfc = data.isGfc;
+        let collection = isGfc ? GfcFormCollection : OdfFormCollection;
+
         if (ulbId && year) {
-            const form = await GfcFormCollection.findOne({ ulb: ulbId, year });
+            const form = await collection.findOne({ ulb: ulbId, year });
             return res.status(200).json({
                 success: true,
                 data: form
@@ -115,5 +122,38 @@ module.exports.getForm = async (req, res) => {
             success: false,
             message: error
         });
+    }
+}
+
+module.exports.action = async ( req, res)=>{
+    const user = req.decoded,
+        data = req.body;
+    data['actionTakenBy'] = user._id;
+    data['actionTakenByRole'] = user.role;
+    const isGfc = data.isGfc;  // flag to check which collection to use 
+    let collection = isGfc ? GfcFormCollection : OdfFormCollection;
+   
+    let condition = {};
+    condition['ulb'] = ObjectId(data.ulb);
+    condition['year'] = ObjectId(data.year);
+    
+    const submittedForm = await collection.findOne(condition)
+    if (!submittedForm){//if submitted form is not found
+        res.status(404).json({
+            success: false,
+            message: "Form not found."
+        });
+    }
+    if (submittedForm.isDraft === false){
+        if (submittedForm.status !== 'PENDING'){
+            //TODO
+        } else {
+
+        }
+    } else {
+        res.status(200).json({
+            status: true,
+            message: "Form not submitted."
+        })
     }
 }
