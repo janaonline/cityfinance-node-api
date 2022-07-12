9db869ed029737a9baa4f989f5e743c03bf03ded
Ashu Gupta <ashu10832@gmail.com>
2022-07-12 14:35:17 +0530

    wip

---
+++ routes/newDashboards/slb.js
@@ -0,0 +1,32 @@
+module.exports.slbFrontPanel = (req,res) => {
+
+    const obj = [
+        {
+            key: 'State with SLB Data',
+            value:''
+        },
+        {
+            key: 'ULBs with SLB Data',
+            value:''
+        },
+        {
+            key: 'Million Population',
+            value:''
+        },
+        {
+            key: 'SLB Indicators',
+            value:''
+        },
+        {
+            key: 'Municipal Corporations',
+            value:''
+        },
+        {
+            key: 'Other Municipalities',
+            value:''
+        }
+    ];
+
+    
+
+}
