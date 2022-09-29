aba0640cebb09f31917977391496c5a1144f1130
Ashu Gupta <ashu10832@gmail.com>
2022-09-29 13:25:49 +0530

    bug fix

--- routes/PropertyTaxOp/service.js
+++ routes/PropertyTaxOp/service.js
@@ -108,7 +108,7 @@ module.exports.createOrUpdateForm = async (req, res)=>{
         let mailOptions = {
           Destination: {
             /* required */
-            ToAddresses: ["dalbeer.kaur@dhwaniris.com"],
+            ToAddresses: [emailAddress],
           },
           Message: {
             /* required */
