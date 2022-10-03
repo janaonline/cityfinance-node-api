986c3898616db76e61854637007eeefc6cbe8253
Aditya Yadav <aditya023.ay@gmail.com>
2022-10-03 11:08:30 +0530

    Removed tooltip for SLB

--- routes/sidemenu/service.js
+++ routes/sidemenu/service.js
@@ -170,7 +170,7 @@ module.exports.get = catchAsync(async (req, res) => {
      }
 
     data.forEach((el,)=> {
-        if( el.category && el.collectionName != "GTC"){
+        if( el.category && el.collectionName != "GTC" && el.collectionName != "SLB"){
             let  flag = 0;
             output.forEach(el2 => {
                 if((el._id).toString() == (Object.keys(el2)[0])){
