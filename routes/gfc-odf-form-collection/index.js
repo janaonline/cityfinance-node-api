934cb2478ba4adce9ef14a282b8bbd31984376df
Ashu Gupta <ashu10832@gmail.com>
2022-10-19 14:59:01 +0530

    gfc odf defunct files

--- routes/gfc-odf-form-collection/index.js
+++ routes/gfc-odf-form-collection/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {createOrUpdateForm, getForm} = require('./service')
+const {createOrUpdateForm, getForm, defunct} = require('./service')
 const { verifyToken } = require('../auth/services/verifyToken')
 
 router.get('/', verifyToken, getForm);
 router.post('/', verifyToken, createOrUpdateForm);
-
+router.get('/defunctGFCODF', defunct);
 module.exports = router;
