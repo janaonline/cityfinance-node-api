3c889b4857168ebedb70a0579c483649d216c6fa
Aditya Yadav <aditya.yadav@dhwaniris.com>
2022-12-12 13:56:25 +0530

    ulbCode and state code added to login

--- routes/auth/services/login.js
+++ routes/auth/services/login.js
@@ -47,6 +47,8 @@ module.exports.login = async (req, res) => {
           stateName: state?.name,
           designation: user?.designation,
           ulb: user.ulb,
+          ulbCode : user.role === "ULB"  ? (user.censusCode ? user.censusCode:  user.sbCode) : "" ,
+          stateCode: user.role === "STATE" || user.role === "ULB" ? state.code : "",
           isUA: role === "ULB" ? ulb.isUA : null,
           isMillionPlus: role === "ULB" ? ulb.isMillionPlus : null,
           isUserVerified2223: user.isVerified2223
