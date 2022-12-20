95ab293d7b0ccddea77166253f640ef51bb53375
Aditya Yadav <aditya.yadav@dhwaniris.com>
2022-12-20 14:57:28 +0530

    added  alertt for 28 slb and 4 slb forms

--- routes/TwentyEightSlbsForm/service.js
+++ routes/TwentyEightSlbsForm/service.js
@@ -5,10 +5,13 @@ const {findPreviousYear} = require('../../util/findPreviousYear')
 const Year = require('../../models/Year')
 const {groupByKey} = require('../../util/group_list_by_key')
 const SLB = require('../../models/XVFcGrantForm')
-const {canTakenAction} = require('../CommonActionAPI/service')
+const {canTakenAction, calculateStatus} = require('../CommonActionAPI/service')
 const Service = require('../../service');
 const {FormNames, YEAR_CONSTANTS} = require('../../util/FormNames');
 const User = require('../../models/User');
+const MasterForm = require('../../models/MasterForm')
+const StatusList = require('../../util/newStatusList')
+const {BackendHeaderHost, FrontendHeaderHost} = require('../../util/envUrl')
 
 function response(form, res, successMsg ,errMsg){
     if(form){
@@ -325,6 +328,7 @@ module.exports.getForm = async (req, res) => {
         if(!(data.ulb && data.design_year)){
             return res.status(400).json({
                 status: false,
+                show: false,
                 message: "Design year and Ulb are mandatory"
             })
         }
@@ -337,6 +341,53 @@ module.exports.getForm = async (req, res) => {
         let prevYearData =   await Year.findOne({
             year : prevYearVal
         }).lean()
+        let masterFormData = await MasterForm.findOne({
+          ulb: data.ulb,
+          design_year: prevYearData._id,
+        }).lean();
+        /* Checking the host header and setting the host variable to the appropriate value. */
+        let host = "";
+        if (req.headers.host === BackendHeaderHost.Demo) {
+          host = FrontendHeaderHost.Demo;
+        }
+        /* Checking if the host is empty, if it is, it will set the host to the req.headers.host. */
+        req.headers.host = host !== "" ? host : req.headers.host;
+        if (masterFormData) {
+          if (masterFormData.history.length > 0) {
+            masterFormData =
+              masterFormData.history[masterFormData.history.length - 1];
+          }
+          let status = calculateStatus(
+            masterFormData.status,
+            masterFormData.actionTakenByRole,
+            !masterFormData.isSubmit,
+            "ULB"
+          );
+          /* Checking the status of the form. If the status is not in the list of statuses, it will
+            return a message. */
+          if (
+            ![
+              StatusList.Under_Review_By_MoHUA,
+              StatusList.Approved_By_MoHUA,
+              StatusList.Approved_By_State,
+            ].includes(status)
+          ) {
+            return res.status(200).json({
+              status: true,
+              show: true,
+              message: `Your Previous Year's form status is - ${
+                status ? status : "Not Submitted"
+              }. Kindly submit form for previous year at - <a href =https://${host}/ulbform/slbs target="_blank">Click here</a> in order to submit form`,
+            });
+          }
+        } else {
+          return res.status(200).json({
+            status: true,
+            show: true,
+            message: `Your Previous Year's form status is - "Not Submitted". Kindly submit form for previous year at - <a href =https://${host}/ulbform/slbs target="_blank">Click here</a> in order to submit form`,
+          });
+        }
+
         let formData = await TwentyEightSlbsForm.findOne(condition, { history: 0} ).lean()
         
         if (formData) {
@@ -344,7 +395,9 @@ module.exports.getForm = async (req, res) => {
             ulb: ObjectId(data.ulb),
             design_year: YEAR_CONSTANTS["21_22"],
           }).lean();
+          let slbDataNotFilled;
         if(slbData){
+          slbDataNotFilled = slbData.blank;
                 formData["data"].forEach((element) => {
                   /* Checking if the element is equal to the previous line item. */
                   if (
@@ -360,36 +413,41 @@ module.exports.getForm = async (req, res) => {
                         ? Number(slbData.waterManagement.houseHoldCoveredPipedSupply
                             ?.target["2223"])
                         : "";
+                        slbDataNotFilled ? element.targetDisable =  false: ""
                       }
                     if (
                         element["indicatorLineItem"].toString() ===
                         PrevLineItem_CONSTANTS[
                             "Per capita supply of water(lpcd)"
                         ]
-                        )
+                        ){
                         element.target_1.value =
                         slbData.waterManagement.waterSuppliedPerDay.hasOwnProperty(
                             "target"
                             )
                             ? Number(slbData.waterManagement.waterSuppliedPerDay?.target["2223"])
                             : "";
+                            slbDataNotFilled ? element.targetDisable =  false: ""
+                        }
                     if (
                         element["indicatorLineItem"].toString() ===
                         PrevLineItem_CONSTANTS[
                             "Extent of non-revenue water (NRW)"
                         ]
-                        )
+                        ){
                         element.target_1.value = slbData.waterManagement.reduction.hasOwnProperty(
                             "target"
                           )
                             ? Number(slbData.waterManagement.reduction?.target["2223"])
                             : "";
+                            slbDataNotFilled ? element.targetDisable =  false: ""
+                        }
                     if (
                         element["indicatorLineItem"].toString() ===
                         PrevLineItem_CONSTANTS[
                             "Coverage of waste water network services"
                         ]
-                        )
+                        ){
                         element.target_1.value =
                         slbData.waterManagement.houseHoldCoveredWithSewerage.hasOwnProperty(
                           "target"
@@ -397,7 +455,9 @@ module.exports.getForm = async (req, res) => {
                           ? Number(slbData.waterManagement.houseHoldCoveredWithSewerage?.target[
                               "2223"
                             ])
-                          : "";                           
+                          : "";  
+                          slbDataNotFilled ? element.targetDisable =  false: ""
+                        }                         
                 });                          
               
         }
@@ -411,8 +471,7 @@ module.exports.getForm = async (req, res) => {
             ),
           });
           formData["data"].forEach((el) => {
-            if (!formData["isDraft"]) {
-              el["targetDisable"] = true;
+            if (!formData["isDraft"]  && formData['actionTakenByRole'] === "ULB" && formData["status"] === "PENDING") {              el["targetDisable"] = true;
               el["actualDisable"] = true;
               formData["popDisable"] = true;
             }
@@ -427,6 +486,7 @@ module.exports.getForm = async (req, res) => {
 
           return res.status(200).json({
             success: true,
+            show: false,
             data: formData,
           });
         } else {
@@ -542,6 +602,7 @@ module.exports.getForm = async (req, res) => {
 
           return res.status(200).json({
             success: true,
+            show: false,
             data: {
               canTakeAction: false,
               data: output,
@@ -552,6 +613,7 @@ module.exports.getForm = async (req, res) => {
     } catch (error) {
         return res.status(400).json({
             status: false,
+            show: false,
             message: error.message
         })
     }
