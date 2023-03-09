ca2f4759ca8bbe3041b26c805158dd119df26cd7
sahil nagpal <sahil.nagpal@dhwaniris.com>
2023-03-09 13:54:58 +0530

    sideMenu form Id added

--- models/Sidemenu.js
+++ models/Sidemenu.js
@@ -23,6 +23,7 @@ const SidemenuSchema = new Schema({
     sequence:{type: Number},
     folderName: {type: String},
     groupSequence: {type: Number},
+    formId :{type:Number,default:null},
     year: {type : Schema.Types.ObjectId, ref: "Year", required: true }
 },{timestamp : {createdAt : "createdAt", updatedAt : "modifiedAt"}});
 
