b0d5ed882a517554ae23e5d13b07ad2193795a3c
sahil nagpal <sahil.nagpal@dhwaniris.com>
2023-03-15 08:40:21 +0530

    tax revenue value changed

--- routes/FiscalRanking/fydynemic.js
+++ routes/FiscalRanking/fydynemic.js
@@ -660,7 +660,10 @@ const fiscalRankingFormJson = () => {
                 "label": "Tax Revenue",
                 "displayPriority": "1",
                 "calculatedFrom": [
-                    "1.1",
+                    '1.1',
+                    '1.2', '1.3',
+                    '1.4', '1.5',
+                    '1.6', '1.7',
                     "1.8"
                 ],
                 "logic": "sum",
