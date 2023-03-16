84c4c146a5f3ee5b9ba6332d2635f02ab8364d4e
Suresh Mali <suresh.mali@dhwaniris.com>
2023-03-16 18:05:28 +0530

    clickup bug fix

--- routes/FiscalRanking/service.js
+++ routes/FiscalRanking/service.js
@@ -715,6 +715,7 @@ exports.getView = async function (req, res, next) {
                   pf['status'] = singleFydata.status;
                   pf['modelName'] = singleFydata.modelName;
                   if (subData[key].calculatedFrom === undefined) {
+                    pf['required'] = singleFydata.status && singleFydata.status == "NA" ? false : true;
                     pf['readonly'] = singleFydata.status && singleFydata.status == "NA" ? true : getReadOnly(singleFydata.status, viewOne.isDraft);
                   }
                   else {
@@ -726,7 +727,9 @@ exports.getView = async function (req, res, next) {
                     pf['status'] = chekFile ? "NA" : "PENDING"
                     pf['modelName'] = chekFile ? "ULBLedger" : ""
                     if (subData[key].calculatedFrom === undefined) {
+                      console.log("chekFile", chekFile)
                       pf['readonly'] = chekFile ? true : false;
+                      pf['required'] = chekFile ? false : true;
                     }
                     else {
                       pf['readonly'] = true;
@@ -739,7 +742,10 @@ exports.getView = async function (req, res, next) {
                   pf['status'] = chekFile ? "NA" : "PENDING";
                   pf['modelName'] = chekFile ? "ULBLedger" : ""
                   if (subData[key].calculatedFrom === undefined) {
+                    console.log("chekFile", chekFile)
+
                     pf['readonly'] = chekFile ? true : false;
+                    pf['required'] = chekFile ? false : true;
                   }
                   else {
                     pf['readonly'] = true;
@@ -763,8 +769,7 @@ exports.getView = async function (req, res, next) {
                     pf['modelName'] = singleFydata ? singleFydata.modelName : "";
                     if (subData[key].calculatedFrom === undefined) {
                       pf['readonly'] = singleFydata && singleFydata.status == "NA" ? true : getReadOnly(singleFydata.status, viewOne.isDraft);
-                    }
-                    else {
+                    } else {
                       pf['readonly'] = true;
                     }
                   }
