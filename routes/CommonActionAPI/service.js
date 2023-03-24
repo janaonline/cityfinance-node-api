f739d44a31a514e4ac6e573ece828081debd32f5
sahil nagpal <sahil.nagpal@dhwaniris.com>
2023-03-24 12:57:17 +0530

    isQuestionData

--- routes/CommonActionAPI/service.js
+++ routes/CommonActionAPI/service.js
@@ -1868,6 +1868,21 @@ function createCustomizedKeys(answerObj,keysMapper){
     return answerObj
 }
 
+function handleIsDraftCases(flattedForm){
+    try{
+        // console.log("flattedForm.isDraft :: ",flattedForm.isDraft)
+        if(flattedForm.isDraft === false){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    catch(err){
+        console.log("error in handleIsDraftCases")
+    }
+}
+
 function handleArrayFields(shortKey,flattedForm,childQuestionData){
     try{
         let valKey = arrFields[shortKey]
@@ -1880,6 +1895,9 @@ function handleArrayFields(shortKey,flattedForm,childQuestionData){
             for(let arrIndex in questionArr){
                 let formObj = createCustomizedKeys(answerObjects[index],keysMapper)
                 let question = questionArr[arrIndex]
+                if(question.isQuestionDisabled !== true){
+                    question.isQuestionDisabled = handleIsDraftCases(flattedForm)
+                }
                 let answer = { label: '', textValue: '', value: '' }
                 handleValues(question,answer,formObj)
                 question.selectedValue = [answer]
@@ -1907,6 +1925,9 @@ async function appendvalues(childQuestionData,flattedForm,shortKey,question){
                         let answer = { label: '', textValue: '', value: '' }
                         await handleValues(obj,answer,flattedForm)
                         obj.selectedValue = [answer]
+                        if(obj.isQuestionDisabled !== true){
+                            obj.isQuestionDisabled = handleIsDraftCases(flattedForm)
+                        }
                         obj.answer = {
                             answer : [answer]
                         }
