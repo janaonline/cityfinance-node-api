41cf8cf83c08eff0e44b7172acb5c5039f681439
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2023-03-29 12:28:28 +0530

    common form submission api

--- routes/CommonFormSubmission/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken } = require('../auth/services/verifyToken')
-
-const Service = require('./service')
-
-
-module.exports = router
