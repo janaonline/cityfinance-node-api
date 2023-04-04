81e17e92234e5e00dfb1a5e86c62571162943c8e
sahil nagpal <sahil.nagpal@dhwaniris.com>
2023-04-04 16:27:16 +0530

    is draft key inside language

--- routes/TwentyEightSlbsForm/middlewares.js
+++ routes/TwentyEightSlbsForm/middlewares.js
@@ -46,8 +46,8 @@ module.exports.changeApiGetForm = async(req,res)=>{
             let obj = formJson.data
             let keysToBeDeleted = ["_id","createdAt","modifiedAt","actionTakenByRole","actionTakenBy","ulb","design_year"]
             obj = await mutuateGetPayload(obj, flattedForm,keysToBeDeleted,role)
-            obj.isDraft =  req?.form?.isDraft
             responseData[0]['language'] = obj
+            responseData[0]['language'][0]['isDraft'] =  req?.form?.isDraft
             response.success = true
             response.data = responseData
             response.message = 'Form Questionare!'
