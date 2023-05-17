720febb401ca0912e1f93514bd2656354cef9dbd
NishantTheProgrammer <knishant113@gmail.com>
2023-05-17 17:59:41 +0530

    added overviewActivities API

--- routes/FiscalRanking/index.js
+++ routes/FiscalRanking/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require('../auth/services/verifyToken')
-const { CreateorUpdate,createForm, getAll,getView,approvedByMohua,getFRforms,createTabsFiscalRanking,actionTakenByMoHua, FRUlbFinancialData, FROverAllUlbData } = require('./service')
+const { CreateorUpdate,createForm, overviewActivities, getAll,getView,approvedByMohua,getFRforms,createTabsFiscalRanking,actionTakenByMoHua, FRUlbFinancialData, FROverAllUlbData } = require('./service')
 
 router.post("/create", verifyToken, CreateorUpdate);
 router.get("/getAll", verifyToken, getAll);
@@ -13,6 +13,7 @@ router.post("/action-by-mohua",verifyToken,actionTakenByMoHua)
 router.post("/create-form",verifyToken,createForm);
 router.get("/csvFRUlb", FRUlbFinancialData);
 router.get('/csvFROverall', FROverAllUlbData);
+router.get('/overviewActivities', verifyToken,  overviewActivities)
 
 
 module.exports = router;
