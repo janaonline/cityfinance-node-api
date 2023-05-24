3af0f9dfe72f8f7ac28560decbb8a188c4d5bc05
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2023-05-24 16:51:31 +0530

    ledger csv commented unused lookups

--- routes/ledger/service.js
+++ routes/ledger/service.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 const ObjectId = require("mongoose").Types.ObjectId;
 const Ulb = require("../../models/Ulb");
+const LineItem = require("../../models/LineItem");
 const RequestLog = require("../../models/RequestLog");
 const DataCollectionForms = require("../../models/DataCollectionForm");
 const AnnualAccount = require("../../models/AnnualAccounts");
@@ -399,7 +400,7 @@ module.exports.getAllUlbLegders = async function (req, res) {
 
 }
 // Get all ledgers present in database in CSV Format
-module.exports.getAllLedgersCsv = function (req, res) {
+module.exports.getAllLedgersCsv = async function (req, res) {
     let filename = "All Ledgers " + (moment().format("DD-MMM-YY HH:MM:SS")) + ".csv";
 
     // Set approrpiate download headers
@@ -408,6 +409,7 @@ module.exports.getAllLedgersCsv = function (req, res) {
     res.write("ULB Name, ULB Code, AMRUT, Head of account,Code, Line Item, Budget year, Budget amount\r\n");
     // Flush the headers before we start pushing the CSV content
     res.flushHeaders();
+    let lineItem = await LineItem.find({ "isActive": true }, { 'name': 1 }).lean()
 
     const cursor = UlbLedger.aggregate([
         {
@@ -418,36 +420,36 @@ module.exports.getAllLedgersCsv = function (req, res) {
                 localField: "ulb"
             }
         },
-        {
-            $lookup: {
-                from: "lineitems",
-                as: "lineitems",
-                foreignField: "_id",
-                localField: "lineItem"
-            }
-        },
-        {
-            $lookup: {
-                from: "states",
-                as: "states",
-                foreignField: "_id",
-                localField: "ulbs.state"
-            }
-        },
-        {
-            $lookup: {
-                from: "ulbtypes",
-                as: "ulbtypes",
-                foreignField: "_id",
-                localField: "ulbs.ulbType"
-            }
-        },
+        // {
+        //     $lookup: {
+        //         from: "lineitems",
+        //         as: "lineitems",
+        //         foreignField: "_id",
+        //         localField: "lineItem"
+        //     }
+        // },
+        // {
+        //     $lookup: {
+        //         from: "states",
+        //         as: "states",
+        //         foreignField: "_id",
+        //         localField: "ulbs.state"
+        //     }
+        // },
+        // {
+        //     $lookup: {
+        //         from: "ulbtypes",
+        //         as: "ulbtypes",
+        //         foreignField: "_id",
+        //         localField: "ulbs.ulbType"
+        //     }
+        // },
         {
             $project: {
                 "ulbs": { $arrayElemAt: ["$ulbs", 0] },
-                "states": { $arrayElemAt: ["$states", 0] },
-                "ulbtypes": { $arrayElemAt: ["$ulbtypes", 0] },
-                "lineitems": { $arrayElemAt: ["$lineitems", 0] },
+                // "states": { $arrayElemAt: ["$states", 0] },
+                // "ulbtypes": { $arrayElemAt: ["$ulbtypes", 0] },
+                // "lineitems": { $arrayElemAt: ["$lineitems", 0] },
                 financialYear: "$financialYear",
                 amount: 1,
                 population: 1
@@ -457,9 +459,9 @@ module.exports.getAllLedgersCsv = function (req, res) {
             $project: {
                 _id: 0,
                 ulb: { $cond: ["$ulbs", "$ulbs", "NA"] },
-                state: { $cond: ["$states", "$states", "NA"] },
-                ulbtypes: { $cond: ["$ulbtypes", "$ulbtypes", "NA"] },
-                line_item: { $cond: ["$lineitems", "$lineitems", "NA"] },
+                // state: { $cond: ["$states", "$states", "NA"] },
+                // ulbtypes: { $cond: ["$ulbtypes", "$ulbtypes", "NA"] },
+                // line_item: { $cond: ["$lineitems", "$lineitems", "NA"] },
                 financialYear: 1,
                 amount: 1,
                 population: 1
@@ -467,6 +469,8 @@ module.exports.getAllLedgersCsv = function (req, res) {
         },
     ]).cursor({ batchSize: 500 }).addCursorFlag('noCursorTimeout', true).exec()
     cursor.on("data", function (el) {
+        let maplineItem = lineItem?.length ? lineItem.find(e => e._id == el.lineItem) : null
+        el['line_item'] = maplineItem;
         if (el.ulb != 'NA') {
             let line_item = el.line_item ? el.line_item.name.toString().replace(/[,]/g, ' | ') : "";
             el.code = el.line_item ? el.line_item.code : "";
