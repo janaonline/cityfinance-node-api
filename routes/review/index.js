7c51a69a76f9b45a2572d5f06e66a1c1078397cf
Suresh Mali <suresh.mali@dhwaniris.com>
2023-06-23 19:50:42 +0530

    limit remove

--- routes/review/index.js
+++ routes/review/index.js
@@ -6,5 +6,5 @@ const verifyToken = require("../auth/services/verifyToken").verifyToken;
 
 router.get("/review", verifyToken, Review.get);
 router.get("/reviewForms", verifyToken, Review2324.get);
-router.get("/csv-download", Review2324.downloadPTOExcel);
+router.get("/ptax-csv-download", Review2324.downloadPTOExcel);
 module.exports = router;
