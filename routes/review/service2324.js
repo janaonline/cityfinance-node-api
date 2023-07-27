574b891ea715aaf59b0810da3a92b08067851ca6
suresh_dhwani <suresh.mali@dhwaniris.com>
2023-07-27 17:09:37 +0530

    space

--- routes/review/service2324.js
+++ routes/review/service2324.js
@@ -347,7 +347,6 @@ function gtcStateFormCSVFormat(obj, res) {
             let str = [...row, ...mainArr, ...tArr].join(',') + "\r\n"
             res.write("\ufeff" + str);
           }
-          
         } else {
           key !== "receiptDate" ? mainArr.push(el[key]) : el[key] ? mainArr.push(formatDate(el[key])) : ""
         }
