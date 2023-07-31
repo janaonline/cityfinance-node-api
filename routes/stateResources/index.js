4697acd84b916d1b4e73a9a39d13a9827709fcc3
NishantTheProgrammer <knishant113@gmail.com>
2023-07-31 19:25:40 +0530

    added loadExcelByUrl

--- routes/stateResources/index.js
+++ routes/stateResources/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { handleDatabaseUpload } = require('./service');
 
 const {
     createOrUpdate, list, deleteById
 } = require("../../service/crud");
 
 router.get('/list', list('StateResource'));
-router.post('/createOrUpdate', createOrUpdate('StateResource'))
+router.post('/createOrUpdate', handleDatabaseUpload, createOrUpdate('StateResource'))
 router.delete('/deleteById/:id', deleteById('StateResource'))
 
 module.exports = router;
