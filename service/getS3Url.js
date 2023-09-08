4438f52b2e506f43dcc8093ec18b320d9a614057
Bhushan Barbuddhe <bhushan.barbuddhe@dhwaniris.com>
2023-09-08 16:37:10 +0530

    Hindi File Name issue resolved

--- service/getS3Url.js
+++ service/getS3Url.js
@@ -4,8 +4,13 @@ module.exports = async function (req, res) {
         if (req.body && Array.isArray(req.body)) {
             let data = [];
             for (single of req.body) {
+                const params = {
+                    folder: single.folder,
+                    file_name: encodeURIComponent(single.file_name), 
+                    mime_type: single.mime_type
+                }
                 // get unresolved signed url
-                data.push(awsService.generateSignedUrl(single));
+                data.push(awsService.generateSignedUrl(params));
             }
             let resolved = await Promise.all(data);
             return res.status(200).json({ success: true, data: resolved });
