39f450a20112ea25cc74b98bb9f24386a96542aa
gautam-dhwani <gautam.kumar@dhwaniris.com>
2023-10-17 14:01:09 +0530

    cron time changed to 6pm for testing.

--- cronjob/cron.js
+++ cronjob/cron.js
@@ -20,7 +20,7 @@ const cronJob = new CronJob(
 );
 
 const frFormFreezes = new CronJob(
-  "00 00 16 * * *",
+  "00 00 18 * * *",
   async function () {
       // SS MM HH DD MM DOFW
       await frFormFreeze("hi you cron is executed successfully!");
