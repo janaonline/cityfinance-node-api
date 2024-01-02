05edc57967bc9a6af5ef6226e94458f7b09490c4
Tamilselvan0275 <tamild0275@gmail.com>
2024-01-02 19:33:30 +0530

    updated the percentage formula and improved performance

--- routes/scoringFR/index.js
+++ routes/scoringFR/index.js
@@ -13,14 +13,15 @@ const { verifyToken } = require('../auth/services/verifyToken');
 
 // ----01. calculate ULB score-----
 router.get('/calculate-score', calculateFRScore);
-// ----02. calculate ULB percentage for 15 indicators and set score for RM, EP, RM and OverAll-----
+// ----02. calculate ULB percentage for 15 indicators and set score for RM, EP, RM and OverAll, 
+// set high and low score for 15 indicator-----
 router.get('/calculate-percentage', calculateFRPercentage);
 // ----03. calculate assessment parameter rank-----
 router.get('/calculate-rank', calculateFRRank);
 // ----04. calculate avg for ass param-----
 router.get('/calculate-avg', calculateFRAverage);
-// ----05. set high and low score for each indicator-----
-router.get('/set-ulb-score', setUlbScore);
+// ----05. set high and low score for each indicator----tobe removed-
+// router.get('/set-ulb-score', setUlbScore);
 // ----06. set state data-----
 router.get('/set-state-data', setStateData);
 
