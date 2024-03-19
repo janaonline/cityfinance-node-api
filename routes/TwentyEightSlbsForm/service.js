dc96610a14ccc4ee87b63d9babf9e4bc93f01be7
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-03-19 17:13:16 +0530

    UA check added #626

--- routes/TwentyEightSlbsForm/service.js
+++ routes/TwentyEightSlbsForm/service.js
@@ -409,7 +409,7 @@ module.exports.getForm = async (req, res, next) => {
             StatusList.Under_Review_By_MoHUA,
             StatusList.Approved_By_MoHUA,
             StatusList.Approved_By_State,
-          ].includes(status)
+          ].includes(status) && ulbData.UA
         ) {
           let msg = userRole === "ULB" ? `Your ${messages[keyName]} Year's SLBs for Water Supply and Sanitation form status is - ${status ? status : "Not Submitted"
             }. Kindly submit form at - <a href =https://${host}/ulbform/ulbform-overview target="_blank">Click here</a> in order to submit form` : `Dear User, The ${ulbData.name} has not yet filled ${messages[keyName]} Year's SLBs for Water Supply and Sanitation form. You will be able to mark your response once STATE approves ${messages[keyName]} year's form.`
@@ -427,15 +427,17 @@ module.exports.getForm = async (req, res, next) => {
           return
         }
       } else {
-        req.json = {
+        if(ulbData.UA){
+          req.json = {
           status: true,
           show: true,
           message: userRole === "ULB" ?
             `Your ${messages[keyName]} Year's SLBs for Water Supply and Sanitation form status is - "Not Submitted". Kindly submit form at - <a href =https://${host}/ulbform/ulbform-overview target="_blank">Click here</a> in order to submit form` :
             `Dear User, The ${ulbData.name} has not yet filled ${messages[keyName]} Year's SLBs for Water Supply and Sanitation form. You will be able to mark your response once STATE approves previous year's form.`,
+          }
+          next();
+          return;
         }
-        next()
-        return
         // return res.status(200).json({
         //   status: true,
         //   show: true,
