11361c53330e013c616b2c6b4ff8a2330a93d76e
Navinder Singh <146955910+navinder-dev@users.noreply.github.com>
2024-09-16 11:18:31 +0530

    Optimise code: ledger dump.

--- routes/ledger/ledger-dump.js
+++ routes/ledger/ledger-dump.js
@@ -1,8 +1,8 @@
-const ULBLedger = require('../../models/UlbLedger');
-const LedgerLog = require('../../models/LedgerLog');
 const LineItem = require('../../models/LineItem');
+const Ulb = require('../../models/Ulb');
 const ExcelJS = require('exceljs');
 const moment = require('moment');
+const ObjectId = require("mongoose").Types.ObjectId
 
 const totOwnRevenueArr = [
     '5dd10c2485c951b54ec1d74b',
@@ -117,21 +117,90 @@ function updateTotals(rowObj, key, lineItemIdStr, amount, categoryArr, categoryK
 
 // Fetch data from DB - Input sheet.
 async function fetchAllData(findQuery) {
-    return ULBLedger.find(findQuery.query, findQuery.projection).cursor();
+    // return ULBLedger.find(findQuery.query, findQuery.projection).cursor();
+    return Ulb.aggregate(
+        [
+            // {
+            //     $match: {
+            //         _id: ObjectId("5dd24b8f91344e2300876ca9")
+            //     }
+            // },
+            {
+                $project: {
+                    _id: 1
+                }
+            },
+            {
+                $lookup: {
+                    from: "ulbledgers",
+                    localField: "_id",
+                    foreignField: "ulb",
+                    as: "ulbledgers"
+                }
+            },
+            {
+                $project: {
+                    "ulbledgers.lineItem": 1,
+                    "ulbledgers.amount": 1,
+                    "ulbledgers.financialYear": 1
+                }
+            }
+        ]
+    ).cursor().exec();
 }
 // Fetch data from DB - Overview sheet.
 async function fetchAllDataOverview() {
-    return LedgerLog.find().cursor();
+    // return LedgerLog.find({ ulb: ObjectId("5dd24b8f91344e2300876ca9") }).cursor();
+    return Ulb.aggregate(
+        [
+            // {
+            //     $match: {
+            //         _id: ObjectId("5dd24b8f91344e2300876ca9")
+            //     }
+            // },
+            {
+                $project: {
+                    _id: 1,
+                    code: 1,
+                    name: 1,
+                    state: 1,
+                    population: 1
+                }
+            },
+            {
+                $lookup: {
+                    from: "ledgerlogs",
+                    localField: "_id",
+                    foreignField: "ulb_id",
+                    as: "ledgerlogs"
+                }
+            },
+            {
+                $project: {
+                    "ledgerlogs.excel_url": 0,
+                    "ledgerlogs.wards": 0,
+                    "ledgerlogs.ulb_code": 0,
+                    // "ledgerlogs.ulb": 0,
+                    "ledgerlogs.state_code": 0,
+                    // "ledgerlogs.state": 0,
+                    "ledgerlogs.population": 0,
+                    "ledgerlogs.lastModifiedAt": 0,
+                    "ledgerlogs.financialYear": 0,
+                    "ledgerlogs.design_year": 0,
+                    "ledgerlogs.area": 0
+                }
+            }
+        ]
+    ).cursor().exec();
 }
 
 module.exports.getLedgerDump = async (req, res) => {
     try {
-
         // Create an array of line items (used to define headers of excel)
         let lineitems = await getLineItemsFromDB();
 
         const findQuery = {};
-        findQuery.query = req.query.financialYear ? { financialYear: req.query.financialYear } : {};
+        findQuery.query = req.query.financialYear ? { financialYear: req.query.financialYear } : { ulb: ObjectId("5dd24b8f91344e2300876ca9") };
         findQuery.projection = { amount: 1, audit_status: 1, financialYear: 1, lineItem: 1, ulb: 1 };
 
         const workbook = new ExcelJS.Workbook();
@@ -146,57 +215,52 @@ module.exports.getLedgerDump = async (req, res) => {
         worksheet.columns = columns;
 
         // Fetch data from DB - Overview sheet.
-        const overviewData = [];
+        const overviewData = {};
         const cursorOverview = await fetchAllDataOverview();
         for (let doc = await cursorOverview.next(); doc != null; doc = await cursorOverview.next()) {
-            overviewData.push(doc);
+            let { _id, population, state, code, name } = doc;
+            for (let eachYearOverviewData of doc.ledgerlogs) {
+                let key = `${doc._id}_${eachYearOverviewData.year}`;
+                overviewData[key] = Object.assign({}, { _id, population, state, code, name }, eachYearOverviewData);
+            }
         }
 
         // Iterate through each document in the cursor (array received from DB) - Input Sheet.
         const cursorInputData = await fetchAllData(findQuery);
         let eachRowObj = {};
         for (let doc = await cursorInputData.next(); doc != null; doc = await cursorInputData.next()) {
-            const key = `${doc.ulb}_${doc.financialYear}`;
+            for (let eachLineItem of doc.ulbledgers) {
+                const key = `${doc._id}_${eachLineItem.financialYear}`;
+                if (!eachRowObj[key]) {
+                    eachRowObj[key] = {
+                        ulb_id: doc._id,
+                        year: eachLineItem.financialYear,
+                    };
+                }
+                const lineItemIdStr = eachLineItem.lineItem.toString();
 
-            if (!eachRowObj[key]) {
-                eachRowObj[key] = {
-                    // ulb_id: doc.ulb,
-                    ulb_code: doc.ulb,
-                    year: doc.financialYear,
-                    audit_status: doc.audit_status
-                };
-            }
-            const lineItemIdStr = doc.lineItem.toString();
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, totOwnRevenueArr, "totOwnRevenue");
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, totRevenueArr, "totRevenue");
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, revExpenditureArr, "revExpenditure");
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, totExpenditureArr, "totExpenditure");
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, capexArr, "capex");
+                updateTotals(eachRowObj, key, lineItemIdStr, eachLineItem.amount, bsSizeArr, "bsSize");
 
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, totOwnRevenueArr, "totOwnRevenue");
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, totRevenueArr, "totRevenue");
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, revExpenditureArr, "revExpenditure");
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, totExpenditureArr, "totExpenditure");
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, capexArr, "capex");
-            updateTotals(eachRowObj, key, lineItemIdStr, doc.amount, bsSizeArr, "bsSize");
-
-            if (doc.amount === null || doc.amount === "") {
-                eachRowObj[key][doc.lineItem] = "N/A";
-            } else {
-                eachRowObj[key][doc.lineItem] = doc.amount;
+                if (eachLineItem.amount !== null && eachLineItem.amount !== "") {
+                    eachRowObj[key][eachLineItem.lineItem] = eachLineItem.amount;
+                }
             }
         }
 
-        // Create a lookup map for overviewData for faster access
-        const overviewLookup = overviewData.reduce((acc, ulbObj) => {
-            const key = `${ulbObj.ulb_id}_${ulbObj.year}`;
-            acc[key] = ulbObj;
-            return acc;
-        }, {});
-
         // Iterate through each row in eachRowObj
         Object.values(eachRowObj).forEach((row) => {
-            const key = `${row.ulb_code}_${row.year}`;
-            const ulbOverviewObj = overviewLookup[key];
+            const key = `${row.ulb_id}_${row.year}`;
+            const ulbOverviewObj = overviewData[key];
 
             if (ulbOverviewObj) {
-                row["ulb_code"] = ulbOverviewObj.ulb_code;
-                row["ulb"] = ulbOverviewObj.ulb;
+                row["ulb_code"] = ulbOverviewObj.code; // from ulbs collection.
+                row["population"] = ulbOverviewObj.population; // from ulbs collection.
+                row["ulb"] = ulbOverviewObj.name; // from ulbs collection.
                 row["state"] = ulbOverviewObj.state;
                 row["audit_status"] = ulbOverviewObj.audit_status;
                 row["audit_firm"] = ulbOverviewObj.audit_firm;
