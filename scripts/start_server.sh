35cd884b89dea60044b0633b67ca5b0850dc09a1
Babu Thomas <babupt@gmail..com>
2024-09-20 12:15:21 +0530

    Start Server script changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 su ubuntu
 cd /var/www/html/cityfinance/cityfinance-node-api
-pm2 start server.js --name cf-node-api
+mv .env_copy .env
+npm i
+/usr/local/nvm/versions/node/v20.17.0/bin/pm2 pm2 start server.js --name cf-node-api
