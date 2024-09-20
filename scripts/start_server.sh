746f44ba036a347e7c737fe62b7b6c385b7d7e59
Babu Thomas <babupt@gmail..com>
2024-09-20 15:50:47 +0530

    deployment path changed in start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
-cd /var/www/html/cityfinance/cf-test-deploy-api
+cd /var/www/html/cityfinance/cityfinance-node-api
 rm .env
 mv .env_copy .env
 npm i
