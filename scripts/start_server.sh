465e4ab389ff4783ebf64a3f0ba12c5af27b0a43
Babu Thomas <babupt@gmail..com>
2024-09-20 15:48:28 +0530

    start script changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-su ubuntu
-cd /var/www/html/cityfinance/cityfinance-node-api
+export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
+cd /var/www/html/cityfinance/cf-test-deploy-api
+rm .env
 mv .env_copy .env
 npm i
 /usr/local/nvm/versions/node/v20.17.0/bin/pm2 pm2 start server.js --name cf-node-api
