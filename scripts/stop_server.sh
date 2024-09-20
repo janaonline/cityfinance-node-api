4e31df543539be8d6e52fa2a21638db451cc6eb1
Babu Thomas <babupt@gmail..com>
2024-09-20 16:39:23 +0530

    stopserver script changed

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
-/usr/local/nvm/versions/node/v20.17.0/bin/pm2 stop all
+ls
+#export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
+#cd /var/www/html/cityfinance/cityfinance-node-api
+#/usr/local/nvm/versions/node/v20.17.0/bin/pm2 stop all
