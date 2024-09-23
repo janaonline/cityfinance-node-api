57f0575b48418a483742e08b82c4affec5924ac6
Babu Thomas <babupt@gmail..com>
2024-09-23 10:34:06 +0530

    After install script changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
 cd /var/www/html/cityfinance/cityfinance-node-api
 rm .env
 mv .env_copy .env
-npm i
+npm i 
 /usr/local/nvm/versions/node/v20.17.0/bin/pm2 start server.js --name cf-node-api
