41c2991166b5c99bb46867b868f1f3bee0e5007a
Babu Thomas <babupt@gmail..com>
2024-09-23 10:44:36 +0530

    Before install script changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-aws s3 cp s3://cf-code-deployment/default /etc/nginx/sites-available/default
-service nginx restart
--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ls
+aws s3 cp s3://cf-code-deployment/default /etc/nginx/sites-available/default
+service nginx restart
