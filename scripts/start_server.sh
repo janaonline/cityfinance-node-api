28622149403b7154a28667988febe3669342680c
Babu Thomas <babupt@gmail..com>
2024-10-18 11:25:46 +0530

    node path changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
+export PATH="$PATH:/usr/local/nvm/versions/node/v20.18.0/bin/"
 cd /var/www/html/cityfinance/cityfinance-node-api
 rm .env
 mv .env_copy .env
 npm i 
-/usr/local/nvm/versions/node/v20.17.0/bin/pm2 start server.js --name cf-node-api
+/usr/local/nvm/versions/node/v20.18.0/bin/pm2 start server.js --name cf-node-api
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
+#export PATH="$PATH:/usr/local/nvm/versions/node/v20.18.0/bin/"
 cd /var/www/html/cityfinance/cityfinance-node-api
 npm i 
-/usr/local/nvm/versions/node/v20.17.0/bin/pm2 stop all
+/usr/local/nvm/versions/node/v20.18.0/bin/pm2 stop all
