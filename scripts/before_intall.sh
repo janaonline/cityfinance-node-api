8eaee06d1dcb0f0e8f47123b578d702efa37d4ce
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 01:04:53 +0530

    updated deploy

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d /var/www/html/cityfinance/citifinance-node-api ]; then
-  echo "Deleting /var/www/html/cityfinance/citifinance-node-api..."
-  rm -rf /var/www/html/cityfinance/citifinance-node-api
+if [ -d /var/www/html/cityfinance/cityfinance-node-api ]; then
+  echo "Deleting /var/www/html/cityfinance/cityfinance-node-api..."
+  rm -rf /var/www/html/cityfinance/cityfinance-node-api
 fi
