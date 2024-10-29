e40319a7566c3354fbd121360b4dbcfc0a1dbacf
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 23:38:37 +0530

    added one more

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 
-rm -rf /var/www/html/cityfinance/cityfinance-node-api/
-mkdir -p /var/www/html/cityfinance/cityfinance-node-api/
-
-mv /var/www/html/cityfinance/node-api-deploy/.* /var/www/html/cityfinance/cityfinance-node-api/ 2>/dev/null
-mv /var/www/html/cityfinance/node-api-deploy/* /var/www/html/cityfinance/cityfinance-node-api/
-
-rm -rf /var/www/html/cityfinance/node-api-deploy/
