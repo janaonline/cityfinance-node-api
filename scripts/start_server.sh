2af42a8459fc0b3bf7ce940be82e18eba6bcd774
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 23:38:06 +0530

    added more to deploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+rm -rf /var/www/html/cityfinance/cityfinance-node-api/
+mkdir -p /var/www/html/cityfinance/cityfinance-node-api/
+
+mv /var/www/html/cityfinance/node-api-deploy/.* /var/www/html/cityfinance/cityfinance-node-api/ 2>/dev/null
+mv /var/www/html/cityfinance/node-api-deploy/* /var/www/html/cityfinance/cityfinance-node-api/
+
+rm -rf /var/www/html/cityfinance/node-api-deploy/
+
 export NVM_DIR="/.nvm"
 export PATH="$NVM_DIR/versions/node/v20.18.0/bin:$PATH"
 export PM2_HOME=/etc/.pm2
