507d160c193e25654a29bc83ceaa4d8ac920f067
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 20:37:28 +0530

    updated deploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export NVM_DIR="/.nvm"
+export PATH="$NVM_DIR/versions/node/v20.18.0/bin:$PATH"
 cd /var/www/html/cityfinance/cityfinance-node-api
 npm install
 # need to add env variables here
