4147f080fd6bc957b292d458cff55f7371b550b8
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 22:30:53 +0530

    fixed deploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-source /root/.bashrc
+export NVM_DIR="/.nvm"
+export PATH="$NVM_DIR/versions/node/v20.18.0/bin:$PATH"
+export PM2_HOME=/etc/.pm2
+
 cd /var/www/html/cityfinance/cityfinance-node-api
 npm install
 # need to add env variables here
