c65f61496f4aa0494ebdaee3d1d56af4c70c63b1
mdshamoon <mohdshamoon841@gmail.com>
2024-11-06 13:19:49 +0530

    updated flags

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-rm -rf /var/www/html/cityfinance/cityfinance-node-api/
-mkdir -p /var/www/html/cityfinance/cityfinance-node-api/
-
-mv /var/www/html/cityfinance/node-api-deploy/.* /var/www/html/cityfinance/cityfinance-node-api/ 2>/dev/null
-mv /var/www/html/cityfinance/node-api-deploy/* /var/www/html/cityfinance/cityfinance-node-api/
-
-rm -rf /var/www/html/cityfinance/node-api-deploy/
-
 export NVM_DIR="/.nvm"
 export PATH="$NVM_DIR/versions/node/v20.18.0/bin:$PATH"
 export PM2_HOME=/etc/.pm2
@@ -15,4 +7,4 @@ export PM2_HOME=/etc/.pm2
 cd /var/www/html/cityfinance/cityfinance-node-api
 npm install
 # need to add env variables here
-pm2 start cityfinance-backend 
+pm2 start --name cityfinance-backend 
