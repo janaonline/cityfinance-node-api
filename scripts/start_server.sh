a01393ad9a784a41ddc5828c82cb204276ee6045
mdshamoon <mohdshamoon841@gmail.com>
2024-11-25 16:25:05 +0530

    added code

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,8 @@ export NVM_DIR="/.nvm"
 export PATH="$NVM_DIR/versions/node/v20.18.0/bin:$PATH"
 export PM2_HOME=/etc/.pm2
 
+
 cd /var/www/html/cityfinance/cityfinance-node-api
 npm install
-# need to add env variables here
+aws secretsmanager get-secret-value --secret-id "test_cityfinance" --query SecretString --output text | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env && echo ".env file created successfully."
 pm2 start server.js --name cityfinance-backend 
