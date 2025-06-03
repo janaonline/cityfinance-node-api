d7b1ec3972d2bcb4b28c43f535e07700d22d0af1
Navinder Singh <navinder.singh@janaagraha.org>
2025-06-03 18:27:14 +0530

    Add cache - national dashboard data availability section.

--- routes_v2/dashboards/national-dashboard/index.js
+++ routes_v2/dashboards/national-dashboard/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const nationalDashboard = require("./nationalDashboard");
+const cacheMiddleware = require('../../../middlewares/cacheMiddleware');
 
-router.get("/data-availability", nationalDashboard.dataAvailability);
+router.get("/data-availability", cacheMiddleware('dashboard'), nationalDashboard.dataAvailability);
 
 module.exports = router;
