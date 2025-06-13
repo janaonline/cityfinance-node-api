4899966fb297ca266e6c10013384fd2fa873e724
Navinder Singh <navinder.singh@janaagraha.org>
2025-06-13 18:47:10 +0530

    Create last modified at api.

--- routes_v2/common/index.js
+++ routes_v2/common/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { getLatestAfsYear } = require('./common.js')
 const Redis = require('../../service/redis');
+const cacheMiddleware = require('../../middlewares/cacheMiddleware');
+const { getLatestAfsYear, getLastModifiedDate } = require('./common.js');
 
 // Get the latest AFS year. @params
 router.get('/get-latest-aa-year', getLatestAfsYear);
 
+// Get the last modified date - ledgers
+router.get(
+	'/get-last-modified-date',
+	cacheMiddleware('dashboard'),
+	getLastModifiedDate
+);
+
 // Delete redis key
 router.get('/delete-redis-key', (req, res) => {
-    Redis.del(req.query.key);
-    return res.send({ message: `'${req.query.key}' deleted successfully!` });
+	Redis.del(req.query.key);
+	return res.send({ message: `'${req.query.key}' deleted successfully!` });
 });
 
 // check if the server is in maintenance mode
 router.get('/maintenance', (req, res) => {
-    const maintenanceMode = process.env.MAINTENANCE_MODE === 'true';
-    res.status(maintenanceMode ? 503 : 200).json({ maintenance: maintenanceMode });
+	const maintenanceMode = process.env.MAINTENANCE_MODE === 'true';
+	res.status(maintenanceMode ? 503 : 200).json({
+		maintenance: maintenanceMode,
+	});
 });
 
 module.exports = router;
