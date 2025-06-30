34b7fd8fbb3da2dee3628614d3fa2b3e76169675
Navinder Singh <navinder.singh@janaagraha.org>
2025-06-30 23:23:28 +0530

    Add balance sheet table structure.

--- routes_v2/dashboards/city-page/bsIs.js
+++ routes_v2/dashboards/city-page/bsIs.js
@@ -4,19 +4,22 @@ const Ulb = require('../../../models/Ulb');
 module.exports.bsIs = async (req, res) => {
 	try {
 		// Validated ulbId.
-		const { ulbId } = req.query;
+		const { ulbId, btnKey } = req.query;
 		if (!ulbId || !ObjectId.isValid(ulbId))
-			return res.status(400).json({ error: 'Invalid or missing ulbId' });
+			throw new Error('Invalid or missing ulbId');
+
+		if (!btnKey) throw new Error('btnKey is required');
 
 		// Fetch data from ledgerLogs.
 		const ledgerData = await Ulb.aggregate(query(ulbId)).exec();
 
 		// Create response.
-		const response = createResponseStructure(ledgerData);
+		const response = createResponseStructure(ledgerData, btnKey);
 
 		return res.status(200).json({
 			success: true,
 			data: response,
+			population: Number(ledgerData?.[0].population),
 		});
 	} catch (err) {
 		console.error('[ULB Fetch Error]', err);
@@ -64,9 +67,12 @@ function query(ulbId) {
 	];
 }
 
-function createResponseStructure(yearWiseLedgerData) {
-	// Deep clone ELEMENTDATA to avoid mutating shared reference
-	const responseStructure = JSON.parse(JSON.stringify(ELEMENTDATA));
+function createResponseStructure(yearWiseLedgerData, btnKey) {
+	let getStructure = BALANCE_SHEET_STRUCTURE;
+	if (btnKey === 'incomeStatement') getStructure = INCOME_STATEMENT_STRUCTURE;
+
+	// Deep clone BS or IS STRUCTURE to avoid mutating shared reference
+	const responseStructure = JSON.parse(JSON.stringify(getStructure));
 
 	for (const yearEntry of yearWiseLedgerData) {
 		const ledgerLog = yearEntry.ledgerLogData;
@@ -76,17 +82,47 @@ function createResponseStructure(yearWiseLedgerData) {
 
 		for (const item of responseStructure) {
 			const lineItemCode = item.code;
-			if (lineItemCode && ledgerLog.lineItems[lineItemCode] !== null)
+
+			// If calculation is true, it represents a group of line items.
+			if (item.calculation) {
+				let result = 0;
+
+				for (const [operation, lineItems] of Object.entries(
+					item.formula
+				)) {
+					for (const lineItem of lineItems) {
+						const value =
+							Number(ledgerLog.lineItems[lineItem]) || 0;
+
+						if (operation === 'add') result += value;
+						else if (operation === 'sub') result -= value;
+					}
+				}
+
+				item[yearKey] = result;
+			} else if (
+				lineItemCode &&
+				ledgerLog.lineItems[lineItemCode] !== null
+			) {
 				item[yearKey] = ledgerLog.lineItems[lineItemCode];
+			}
 		}
 	}
 
 	return responseStructure;
 }
+/**
+ * isHeader: Boolean - Indicates whether the item is a header. Used by the front-end to skip rendering a dash ("-") in the table.
+ * reportType: 'detailed' | 'summary' - Specifies the type of report. Helps the front-end filter content based on the selected report type.
+ * calculation: Boolean - Flags whether the item requires back-end calculation based on grouped line items.
+ * formula: { add: [], sub: [] } - Defines the calculation logic using lists of line item keys to add and subtract.
+ * info: Used on front-end to add info icon.
+ */
 
-const ELEMENTDATA = [
+const INCOME_STATEMENT_STRUCTURE = [
 	{
 		code: null,
+		isHeader: true,
 		lineItem: 'A.Income',
 		class: 'fw-bold',
 	},
@@ -117,6 +153,8 @@ const ELEMENTDATA = [
 	{
 		code: '120-150',
 		reportType: 'summary',
+		calculation: true,
+		formula: { add: [120, 130, 140, 150] },
 		lineItem: 'Non-Tax Revenue',
 	},
 	{
@@ -141,6 +179,8 @@ const ELEMENTDATA = [
 	{
 		code: '170-180',
 		reportType: 'summary',
+		calculation: true,
+		formula: { add: [170, 171, 180] },
 		lineItem: 'Other Income',
 	},
 	{
@@ -151,11 +191,27 @@ const ELEMENTDATA = [
 		code: null,
 		key: 'totalIncome',
 		lineItem: 'Total Income (A)',
+		calculation: true,
+		formula: {
+			add: [
+				'110',
+				'120',
+				'130',
+				'140',
+				'150',
+				'160',
+				'170',
+				'171',
+				'180',
+				'100',
+			],
+		},
 		info: 'Calculation: (110 + 120 + 130 + 140 + 150 + 160 + 170 + 171 + 180 + 100)',
 		class: 'fw-bold',
 	},
 	{
 		code: null,
+		isHeader: true,
 		lineItem: 'B.Expenditure',
 		class: 'fw-bold',
 	},
@@ -201,6 +257,8 @@ const ELEMENTDATA = [
 	{
 		code: '250, 270-272',
 		reportType: 'summary',
+		calculation: true,
+		formula: { add: ['250', '270', '271', '272'] },
 		lineItem: 'Other Income',
 	},
 	{
@@ -210,11 +268,53 @@ const ELEMENTDATA = [
 	{
 		code: null,
 		lineItem: 'Total Expenditure(B)',
+		calculation: true,
+		formula: {
+			add: [
+				'210',
+				'220',
+				'230',
+				'240',
+				'250',
+				'260',
+				'270',
+				'271',
+				'272',
+				'200',
+			],
+		},
 		info: 'Calculation: (210 + 220 + 230 + 240 + 250 + 260 + 270 + 271 + 272 + 200)',
 		class: 'fw-bold',
 	},
 	{
 		code: null,
+		calculation: true,
+		formula: {
+			add: [
+				'110',
+				'120',
+				'130',
+				'140',
+				'150',
+				'160',
+				'170',
+				'171',
+				'180',
+				'100',
+			],
+			sub: [
+				'210',
+				'220',
+				'230',
+				'240',
+				'250',
+				'260',
+				'270',
+				'271',
+				'272',
+				'200',
+			],
+		},
 		lineItem:
 			'Gross Surplus/(Deficit) of Income over Expenditure before Prior Period Items (C) (A-B)',
 		class: 'fw-bold',
@@ -225,6 +325,34 @@ const ELEMENTDATA = [
 	},
 	{
 		code: null,
+		calculation: true,
+		formula: {
+			add: [
+				'110',
+				'120',
+				'130',
+				'140',
+				'150',
+				'160',
+				'170',
+				'171',
+				'180',
+				'100',
+				'280',
+			],
+			sub: [
+				'210',
+				'220',
+				'230',
+				'240',
+				'250',
+				'260',
+				'270',
+				'271',
+				'272',
+				'200',
+			],
+		},
 		lineItem:
 			'Gross Surplus/(Deficit) of Income over Expenditure after Prior Period Items item(D) (C+280)',
 		class: 'fw-bold',
@@ -235,7 +363,481 @@ const ELEMENTDATA = [
 	},
 	{
 		code: null,
+		calculation: true,
+		formula: {
+			add: [
+				'110',
+				'120',
+				'130',
+				'140',
+				'150',
+				'160',
+				'170',
+				'171',
+				'180',
+				'100',
+				'280',
+				'290',
+			],
+			sub: [
+				'210',
+				'220',
+				'230',
+				'240',
+				'250',
+				'260',
+				'270',
+				'271',
+				'272',
+				'200',
+			],
+		},
 		lineItem: 'Net Surplus/(Deficit) carried over (E) (D+290)',
 		class: 'fw-bold',
+	},
+];
+
+const BALANCE_SHEET_STRUCTURE = [
+	{
+		code: null,
+		isHeader: true,
+		lineItem: 'A. Liabilities',
+		class: 'fw-bold',
+	},
+	{
+		code: null,
+		isHeader: true,
+		reportType: 'detailed',
+		lineItem: 'I. Reserves & Surplus',
+		class: 'fw-bold',
+	},
+	{
+		code: 310,
+		lineItem: 'Municipal (General) Fund',
+		reportType: 'detailed',
+	},
+	{
+		code: 311,
+		lineItem: 'Earmarked Funds',
+		reportType: 'detailed',
+	},
+	{
+		code: 312,
+		lineItem: 'Reserves',
+		reportType: 'detailed',
+	},
+	{
+		code: '310-312',
+		lineItem: 'Reserves & Surplus',
+		reportType: 'summary',
+		calculation: true,
+		formula: { add: ['310', '311', '312'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Reserves & Surplus (I)',
+		calculation: true,
+		formula: { add: ['310', '311', '312'] },
+		info: 'Calculation: (310 + 311 + 312)',
+	},
+	{
+		code: null,
+		isHeader: true,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'II. Grants , Contribution for specific purposes',
+	},
+	{
+		code: 320,
+		lineItem: 'Grants, Contribution for Specific purposes',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		lineItem: 'Total Grants , Contribution for specific purposes (II)',
+		reportType: 'detailed',
+		info: 'Calculation: (320)',
+		calculation: true,
+		formula: { add: ['320'] },
+	},
+	{
+		code: null,
+		isHeader: true,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'III. Loans',
+	},
+	{
+		code: 330,
+		reportType: 'detailed',
+		lineItem: 'Secured Loans',
+	},
+	{
+		code: 331,
+		reportType: 'detailed',
+		lineItem: 'Unsecured Loans',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Loans (III)',
+		info: 'Calculation: (330 + 331)',
+		calculation: true,
+		formula: { add: ['330', '331'] },
+	},
+	{
+		code: '330-331',
+		reportType: 'summary',
+		lineItem: 'Loans',
+		calculation: true,
+		formula: { add: ['330', '331'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'IV. Current Liabilities and Provisions',
+		isHeader: true,
+	},
+	{
+		code: 340,
+		reportType: 'detailed',
+		lineItem: 'Deposits received',
+	},
+	{
+		code: 341,
+		reportType: 'detailed',
+		lineItem: 'Deposit Works',
+	},
+	{
+		code: 350,
+		reportType: 'detailed',
+		lineItem: 'Other Liabilities (Sundry Creditors)',
+	},
+	{
+		code: 360,
+		reportType: 'detailed',
+		lineItem: 'Provisions',
+	},
+	{
+		code: '340-360',
+		reportType: 'summary',
+		lineItem: 'Current Liabilities and Provisions',
+		calculation: true,
+		formula: { add: ['340', '341', '350', '360'] },
+	},
+	{
+		code: 300,
+		lineItem: 'Others',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Current Liabilities and Provisions (IV)',
+		info: 'Calculation: (340 + 341 + 350 + 360 + 300)',
+		calculation: true,
+		formula: { add: ['340', '341', '350', '360', '300'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Liabilities',
+		info: 'Calculation: (I + II + III + IV)',
+		calculation: true,
+		formula: {
+			add: [
+				'310',
+				'311',
+				'312',
+				'320',
+				'330',
+				'331',
+				'340',
+				'341',
+				'350',
+				'360',
+				'300',
+			],
+		},
+	},
+	{
+		code: null,
+		reportType: 'summary',
+		lineItem: 'Total Liabilities (A)',
+		info: 'Calculation: (310 + 311 + 312 + 320 + 330 + 331 + 340 + 341 + 350 + 360 + 300)',
+		class: 'fw-bold',
+		calculation: true,
+		formula: {
+			add: [
+				'310',
+				'311',
+				'312',
+				'320',
+				'330',
+				'331',
+				'340',
+				'341',
+				'350',
+				'360',
+				'300',
+			],
+		},
+	},
+	{
+		code: null,
+		lineItem: 'B. Assets',
+		isHeader: true,
+		class: 'fw-bold',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		lineItem: 'I. Fixed Assets',
+		reportType: 'detailed',
+		isHeader: true,
+	},
+	{
+		code: 410,
+		reportType: 'detailed',
+		lineItem: 'Gross Block',
+	},
+	{
+		code: 411,
+		reportType: 'detailed',
+		lineItem: 'Accumulated Depreciation',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		lineItem: 'Net Block',
+		reportType: 'detailed',
+		info: 'Calculation: (410 - 411)',
+		calculation: true,
+		formula: { sub: ['410', '411'] },
+	},
+	{
+		code: 412,
+		reportType: 'detailed',
+		lineItem: 'Capital Work-in-progress',
+	},
+	{
+		code: '410-412',
+		reportType: 'summary',
+		lineItem: 'Fixed Assets',
+		info: 'Calculation: (410 - 411 + 412)',
+		calculation: true,
+		formula: {
+			add: ['410', '412'],
+			sub: ['411'],
+		},
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Fixed Assets (I)',
+		info: 'Calculation: (410 - 411 + 412)',
+		calculation: true,
+		formula: {
+			add: ['410', '412'],
+			sub: ['411'],
+		},
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'II. Investments',
+		isHeader: true,
+	},
+	{
+		code: 420,
+		reportType: 'detailed',
+		lineItem: 'Investment - General Fund',
+	},
+	{
+		code: 421,
+		reportType: 'detailed',
+		lineItem: 'Investment - Other Funds',
+	},
+	{
+		code: '420-421',
+		reportType: 'summary',
+		lineItem: 'Investments',
+		calculation: true,
+		formula: { add: ['420', '421'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Investments (II)',
+		info: 'Calculation: (420 + 421)',
+		calculation: true,
+		formula: { add: ['420', '421'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'III. Current Assets, Loans and Advances',
+		isHeader: true,
+	},
+	{
+		code: 430,
+		reportType: 'detailed',
+		lineItem: 'Stock in Hand (Inventories)',
+	},
+	{
+		code: 431,
+		reportType: 'detailed',
+		lineItem: 'Sundry Debtors (Receivables)',
+	},
+	{
+		code: 432,
+		reportType: 'detailed',
+		lineItem: 'Accumulated Provisions against Bad and Doubtful Receivables',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Net amount outstanding (i)',
+		info: 'Calculation: (430 + 431 - 432)',
+		calculation: true,
+		formula: {
+			add: ['430', '431'],
+			sub: ['432'],
+		},
+	},
+	{
+		code: 440,
+		reportType: 'detailed',
+		lineItem: 'Prepaid Expenses',
+	},
+	{
+		code: 450,
+		reportType: 'detailed',
+		lineItem: 'Cash and Bank Balance',
+	},
+	{
+		code: 460,
+		reportType: 'detailed',
+		lineItem: 'Loans, Advances and Deposits',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Net amount outstanding (ii)',
+		info: 'Calculation: (440 + 450 + 460)',
+		calculation: true,
+		formula: { add: ['440', '450', '460'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Current Assets, Loans and Advances (III)',
+		info: 'Calculation: i + ii',
+		calculation: true,
+		formula: {
+			add: ['430', '431', '440', '450', '460', '420', '421'],
+			sub: ['432'],
+		},
+	},
+	{
+		code: '430-461',
+		reportType: 'summary',
+		lineItem: 'Current Assets, Loans and Advances',
+	},
+	{
+		code: null,
+		isHeader: true,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'IV. Other Assets',
+	},
+	{
+		code: 470,
+		reportType: 'detailed',
+		lineItem: 'Other Assets',
+	},
+	{
+		code: 480,
+		reportType: 'detailed',
+		lineItem: 'Miscellaneous Expenditure (to the extent not written off)',
+	},
+	{
+		code: '470-480',
+		reportType: 'summary',
+		lineItem: 'Other Assets',
+		calculation: true,
+		formula: { add: ['470', '480'] },
+	},
+	{
+		code: 400,
+		lineItem: 'Others',
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Other Assets (IV)',
+		info: 'Calculation: (470 + 480 + 400)',
+		calculation: true,
+		formula: { add: ['470', '480', '400'] },
+	},
+	{
+		code: null,
+		class: 'fw-bold',
+		reportType: 'detailed',
+		lineItem: 'Total Assets',
+		info: 'Calculation: (I + II + III + IV)',
+		calculation: true,
+		formula: {
+			add: [
+				'470',
+				'480',
+				'400',
+				'430',
+				'431',
+				'440',
+				'450',
+				'460',
+				'410',
+				'412',
+			],
+			sub: ['432', '411'],
+		},
+	},
+	{
+		code: null,
+		reportType: 'summary',
+		lineItem: 'Total Assets (B)',
+		class: 'fw-bold',
+		calculation: true,
+		formula: {
+			add: [
+				'410',
+				'412',
+				'420',
+				'421',
+				'430',
+				'431',
+				'440',
+				'450',
+				'460',
+				'470',
+				'480',
+				'400',
+			],
+			sub: ['411', '432'],
+		},
 	},
 ];
