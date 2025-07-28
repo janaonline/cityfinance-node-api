56e54e34712aa12f0cb41a5202ab2891933e9aa0
Navinder Singh <navinder.singh@janaagraha.org>
2025-07-28 13:23:18 +0530

    Add timeout middleware.

--- routes_v2/budget-documents/index.js
+++ routes_v2/budget-documents/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const service = require('./service');
-const { verifyToken } = require('../../routes/auth/services/verifyToken')
+const { verifyToken } = require('../../routes/auth/services/verifyToken');
+const withTimeout = require('../../middlewares/customTimeOutMiddleware');
 
 router.get('/getYearsData', service.getYearsData);
 router.post('/updatePdfs', service.updatePdfs);
 // Migrate data from FiscalRanking to budget collection.
-router.get('/convertJson', verifyToken, service.convertJson);
+router.get('/convertJson', verifyToken, withTimeout(120000), service.convertJson);
 router.get('/getValidations', service.getValidations);
 router.post('/uploadDataBulkPdf', service.uploadDataPDF);
 router.get('/getUlbList', service.getUlbList);
