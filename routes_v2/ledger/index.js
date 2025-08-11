d35903c042e33f2816062ed9df4a6d7c8b983c24
Kushal-MP08 <kushal.mp@janaagrha.org>
2025-08-11 13:07:46 +0530

    api changes

--- routes_v2/ledger/index.js
+++ routes_v2/ledger/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const verifyToken = require('../../routes/auth/services/verifyToken').verifyToken;
 const { transferUlbLedgersToLedgerLogs } = require('./cron');
 const { getLedgerDump } = require('./ledger-dump');
-const { getIndicators,createIndicators,getCityDasboardIndicators } = require('./indicators');
+const { getIndicators,createIndicators,getCityDasboardIndicators,getYearsDynamic,getFaqs } = require('./indicators');
 
 router.post('/transfer-ulbLedgers', verifyToken, transferUlbLedgersToLedgerLogs);
 router.get('/get-indicatorTotals', getIndicators);
 router.get('/getCityDasboardIndicators', getCityDasboardIndicators);
+router.get('/getYearsDynamic', getYearsDynamic);
+router.get('/getFaqs', getFaqs);
 router.post('/create-indicators', createIndicators);
 router.get('/getLedgerDump', getLedgerDump);
 
