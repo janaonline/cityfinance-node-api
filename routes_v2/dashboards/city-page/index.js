5d2e7045393042e87e9735c48ce66bba40d0800a
Navinder Singh <navinder.singh@janaagraha.org>
2025-11-19 13:32:29 +0530

    Create API remplate for fetching slb data - 2021-22 onwards.

--- routes_v2/dashboards/city-page/index.js
+++ routes_v2/dashboards/city-page/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { cityDetails } = require('./cityDetails');
 const { bsIs } = require('./bsIs');
 const { financialIndicators } = require('./financialIndicators');
+const { slbIndicators } = require('./slbIndicators');
 const cacheMiddleware = require('../../../middlewares/cacheMiddleware');
 
 router.get('/city-details', cacheMiddleware('dashboard'), cityDetails);
 router.get('/bs-is', cacheMiddleware('dashboard'), bsIs);
 router.post('/financial-indicators', cacheMiddleware('dashboard'), financialIndicators);
+router.get('/slb-indicators', slbIndicators);
 
 module.exports = router;
---
+++ routes_v2/dashboards/city-page/slbIndicators.js
@@ -0,0 +1,6 @@
+module.exports.slbIndicators = async (req, res) => {
+    return res.status(200).json({
+        success: true,
+        data: [],
+    });
+}
