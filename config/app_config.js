cd915e291fdf323eae473534bbe2580464a83a12
Navinder Singh <navinder.singh@janaagraha.org>
2026-05-25 16:58:13 +0530

    feat(file-token): update default token expiration to 72 hours

--- config/app_config.js
+++ config/app_config.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	FILE_DOWNLOAD: {
 		TOKEN_SECRET: process.env.SECRET,
-		LINK_TTL_MS: parseInt(process.env.FILE_DOWNLOAD_LINK_TTL_MS, 10) || 24 * 60 * 60 * 1000,
+		LINK_TTL_MS: parseInt(process.env.FILE_DOWNLOAD_LINK_TTL_MS, 10) || 72 * 60 * 60 * 1000,
 		APP_BASE_URL: (process.env.BASEURL || 'http://localhost:8080').replace(/\/$/, ''),
 		APP_BASE_URL2: (process.env.BASEURL2 || 'http://localhost:3000').replace(/\/$/, ''),
 	},
